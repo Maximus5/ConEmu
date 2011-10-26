16b739d3fa397ca35b75c208c084b79cab2ad3ae
ConEmu.Maximus5 <ConEmu.Maximus5@92116c04-ab5e-ca4c-3a8b-6f4eadf4b4a1>
2011-10-26 20:39:20 +0000

    [svn] wiki pages
    git-svn-id: https://conemu-maximus5.googlecode.com/svn@194 92116c04-ab5e-ca4c-3a8b-6f4eadf4b4a1

---
+++ src/ConEmuHk/UserImp.cpp
@@ -0,0 +1,668 @@
+
+/*
+Copyright (c) 2011 Maximus5
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include <windows.h>
+#include "UserImp.h"
+#include "../common/defines.h"
+#include "../common/MAssert.h"
+
+UserImp* user = NULL;
+
+// Вызывать LoadLibrary из DllMain (откуда стартует UserImp::loadExports) нельзя
+// Поэтому - только GetModuleHandle, и если он уже загружен в process space - грузим экспорты.
+// Однако, чтобы при динамической линковке не обломаться, впоследствии нужно
+// подтвердить (накрутить счетчик) использование user32 через LoadLibrary.
+BOOL UserImp::loadExports(BOOL abAllowLoadLibrary)
+{
+	if (!hUser32)
+	{
+		if (abAllowLoadLibrary)
+		{
+			hUser32 = LoadLibrary(L"user32.dll");
+			bUserLoaded = TRUE;
+		}
+		else
+		{
+			hUser32 = GetModuleHandle(L"user32.dll");
+		}
+		
+		if (hUser32)
+		{
+			allowSetForegroundWindow_f = (allowSetForegroundWindow_t)GetProcAddress(hUser32, "AllowSetForegroundWindow");
+			setForegroundWindow_f = (setForegroundWindow_t)GetProcAddress(hUser32, "SetForegroundWindow");
+			getForegroundWindow_f = (getForegroundWindow_t)GetProcAddress(hUser32, "GetForegroundWindow");
+			getWindowThreadProcessId_f = (getWindowThreadProcessId_t)GetProcAddress(hUser32, "GetWindowThreadProcessId");
+			setWindowPos_f = (setWindowPos_t)GetProcAddress(hUser32, "SetWindowPos");
+			getWindowLongPtrW_f = (getWindowLongPtrW_t)GetProcAddress(hUser32, WIN3264TEST("GetWindowLongW","GetWindowLongPtrW"));
+			setWindowLongPtrW_f = (setWindowLongPtrW_t)GetProcAddress(hUser32, WIN3264TEST("SetWindowLongW","SetWindowLongPtrW"));
+			getParent_f = (getParent_t)GetProcAddress(hUser32, "GetParent");
+			setParent_f = (setParent_t)GetProcAddress(hUser32, "SetParent");
+			getWindowRect_f = (getWindowRect_t)GetProcAddress(hUser32, "GetWindowRect");
+			systemParametersInfoW_f = (systemParametersInfoW_t)GetProcAddress(hUser32, "SystemParametersInfoW");
+			setWindowTextW_f = (setWindowTextW_t)GetProcAddress(hUser32, "SetWindowTextW");
+			endDialog_f = (endDialog_t)GetProcAddress(hUser32, "EndDialog");
+			postMessageW_f = (postMessageW_t)GetProcAddress(hUser32, "PostMessageW");
+			sendMessageW_f = (sendMessageW_t)GetProcAddress(hUser32, "SendMessageW");
+			dialogBoxIndirectParamW_f = (dialogBoxIndirectParamW_t)GetProcAddress(hUser32, "DialogBoxIndirectParamW");
+			getClassNameW_f = (getClassNameW_t)GetProcAddress(hUser32, "GetClassNameW");
+			getClientRect_f = (getClientRect_t)GetProcAddress(hUser32, "GetClientRect");
+			getMenu_f = (getMenu_t)GetProcAddress(hUser32, "GetMenu");
+			attachThreadInput_f = (attachThreadInput_t)GetProcAddress(hUser32, "AttachThreadInput");
+			getFocus_f = (getFocus_t)GetProcAddress(hUser32, "GetFocus");
+			getWindowRect_f = (getWindowRect_t)GetProcAddress(hUser32, "GetWindowRect");
+			isWindow_f = (isWindow_t)GetProcAddress(hUser32, "IsWindow");
+			isWindowVisible_f = (isWindowVisible_t)GetProcAddress(hUser32, "IsWindowVisible");
+			showWindow_f = (showWindow_t)GetProcAddress(hUser32, "ShowWindow");
+			getKeyState_f = (getKeyState_t)GetProcAddress(hUser32, "GetKeyState");
+			callNextHookEx_f = (callNextHookEx_t)GetProcAddress(hUser32, "CallNextHookEx");
+			setWindowsHookExW_f = (setWindowsHookExW_t)GetProcAddress(hUser32, "SetWindowsHookExW");
+			unhookWindowsHookEx_f = (unhookWindowsHookEx_t)GetProcAddress(hUser32, "UnhookWindowsHookEx");
+			_ASSERTEX(allowSetForegroundWindow_f && setForegroundWindow_f && getForegroundWindow_f && getWindowThreadProcessId_f);
+			_ASSERTEX(setWindowPos_f && getWindowLongPtrW_f && setWindowLongPtrW_f && getParent_f && setParent_f && getWindowRect_f);
+			_ASSERTEX(systemParametersInfoW_f && setWindowTextW_f && endDialog_f && postMessageW_f && sendMessageW_f);
+			_ASSERTEX(dialogBoxIndirectParamW_f && getClassNameW_f && getClientRect_f && getMenu_f && attachThreadInput_f);
+			_ASSERTEX(getFocus_f && getWindowRect_f && isWindow_f && isWindowVisible_f && showWindow_f);
+			_ASSERTEX(getKeyState_f && callNextHookEx_f && setWindowsHookExW_f && unhookWindowsHookEx_f);
+		}
+	}
+	
+	if (abAllowLoadLibrary && !bUserLoaded)
+	{
+		hUser32 = LoadLibrary(L"user32.dll");
+	}
+	
+	return (hUser32 != NULL);
+}
+
+void UserImp::setAllowLoadLibrary()
+{
+	bAllowLoadLibrary = TRUE;
+}
+
+BOOL UserImp::allowSetForegroundWindow(DWORD dwProcessId)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (allowSetForegroundWindow_f)
+	{
+		lbRc = allowSetForegroundWindow_f(dwProcessId);
+	}
+	else
+	{
+		_ASSERTEX(allowSetForegroundWindow_f!=NULL);
+	}
+	return lbRc;
+}
+
+BOOL UserImp::setForegroundWindow(HWND hWnd)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (setForegroundWindow_f)
+	{
+		lbRc = setForegroundWindow_f(hWnd);
+	}
+	else
+	{
+		_ASSERTEX(setForegroundWindow_f!=NULL);
+	}
+	return lbRc;
+}
+
+HWND UserImp::getForegroundWindow()
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HWND hRc = NULL;
+	if (getForegroundWindow_f)
+	{
+		hRc = getForegroundWindow_f();
+	}
+	else
+	{
+		_ASSERTEX(getForegroundWindow_f!=NULL);
+	}
+	return hRc;
+}
+
+DWORD UserImp::getWindowThreadProcessId(HWND hWnd, LPDWORD lpdwProcessId)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	DWORD lRc = 0;
+	if (getWindowThreadProcessId_f)
+	{
+		lRc = getWindowThreadProcessId_f(hWnd, lpdwProcessId);
+	}
+	else
+	{
+		_ASSERTEX(getWindowThreadProcessId_f!=NULL);
+	}
+	return lRc;
+}
+
+BOOL UserImp::setWindowPos(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx, int cy, UINT uFlags)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (setWindowPos_f)
+	{
+		lbRc = setWindowPos_f(hWnd, hWndInsertAfter, X, Y, cx, cy, uFlags);
+	}
+	else
+	{
+		_ASSERTEX(setWindowPos_f!=NULL);
+	}
+	return lbRc;
+}
+
+LONG_PTR UserImp::getWindowLongPtrW(HWND hWnd, int nIndex)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	LONG_PTR lRc = 0;
+	if (getWindowLongPtrW_f)
+	{
+		lRc = getWindowLongPtrW_f(hWnd, nIndex);
+	}
+	else
+	{
+		_ASSERTEX(getWindowLongPtrW_f!=NULL);
+	}
+	return lRc;
+}
+
+LONG_PTR UserImp::setWindowLongPtrW(HWND hWnd, int nIndex, LONG_PTR dwNewLong)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	LONG_PTR lRc = 0;
+	if (setWindowLongPtrW_f)
+	{
+		lRc = setWindowLongPtrW_f(hWnd, nIndex, dwNewLong);
+	}
+	else
+	{
+		_ASSERTEX(setWindowLongPtrW_f!=NULL);
+	}
+	return lRc;
+}
+
+HWND UserImp::getParent(HWND hWnd)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HWND hRc = NULL;
+	if (getParent_f)
+	{
+		hRc = getParent_f(hWnd);
+	}
+	else
+	{
+		_ASSERTEX(getParent_f!=NULL);
+	}
+	return hRc;
+}
+
+HWND UserImp::setParent(HWND hWndChild, HWND hWndNewParent)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HWND hRc = NULL;
+	if (setParent_f)
+	{
+		hRc = setParent_f(hWndChild, hWndNewParent);
+	}
+	else
+	{
+		_ASSERTEX(setParent_f!=NULL);
+	}
+	return hRc;
+}
+
+BOOL UserImp::getWindowRect(HWND hWnd, LPRECT lpRect)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL bRc = FALSE;
+	if (getWindowRect_f)
+	{
+		bRc = getWindowRect_f(hWnd, lpRect);
+	}
+	else
+	{
+		_ASSERTEX(getWindowRect_f!=NULL);
+	}
+	return bRc;
+}
+
+BOOL UserImp::systemParametersInfoW(UINT uiAction, UINT uiParam, PVOID pvParam, UINT fWinIni)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL bRc = FALSE;
+	if (systemParametersInfoW_f)
+	{
+		bRc = systemParametersInfoW_f(uiAction, uiParam, pvParam, fWinIni);
+	}
+	else
+	{
+		_ASSERTEX(systemParametersInfoW_f!=NULL);
+	}
+	return bRc;
+}
+
+BOOL UserImp::setWindowTextW(HWND hWnd, LPCWSTR lpString)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL bRc = FALSE;
+	if (setWindowTextW_f)
+	{
+		bRc = setWindowTextW_f(hWnd, lpString);
+	}
+	else
+	{
+		_ASSERTEX(setWindowTextW_f!=NULL);
+	}
+	return bRc;
+}
+
+BOOL UserImp::endDialog(HWND hDlg, INT_PTR nResult)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL bRc = FALSE;
+	if (endDialog_f)
+	{
+		bRc = endDialog_f(hDlg, nResult);
+	}
+	else
+	{
+		_ASSERTEX(endDialog_f!=NULL);
+	}
+	return bRc;
+}
+
+BOOL UserImp::postMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (postMessageW_f)
+	{
+		lbRc = postMessageW_f(hWnd, Msg, wParam, lParam);
+	}
+	else
+	{
+		_ASSERTEX(postMessageW_f!=NULL);
+	}
+	return lbRc;
+}
+
+LRESULT UserImp::sendMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	LRESULT lRc = 0;
+	if (sendMessageW_f)
+	{
+		lRc = sendMessageW_f(hWnd, Msg, wParam, lParam);
+	}
+	else
+	{
+		_ASSERTEX(sendMessageW_f!=NULL);
+	}
+	return lRc;
+}
+
+INT_PTR UserImp::dialogBoxIndirectParamW(HINSTANCE hInstance, LPCDLGTEMPLATEW hDialogTemplate, HWND hWndParent, DLGPROC lpDialogFunc, LPARAM dwInitParam)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	INT_PTR nRc = 0;
+	if (dialogBoxIndirectParamW_f)
+	{
+		nRc = dialogBoxIndirectParamW_f(hInstance, hDialogTemplate, hWndParent, lpDialogFunc, dwInitParam);
+	}
+	else
+	{
+		_ASSERTEX(dialogBoxIndirectParamW_f!=NULL);
+	}
+	return nRc;
+}
+
+int UserImp::getClassNameW(HWND hWnd, LPTSTR lpClassName, int nMaxCount)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	int lRc = 0;
+	if (getClassNameW_f)
+	{
+		lRc = getClassNameW_f(hWnd, lpClassName, nMaxCount);
+	}
+	else
+	{
+		_ASSERTEX(getClassNameW_f!=NULL);
+	}
+	return lRc;
+}
+
+BOOL UserImp::getClientRect(HWND hWnd, LPRECT lpRect)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (getClientRect_f)
+	{
+		lbRc = getClientRect_f(hWnd, lpRect);
+	}
+	else
+	{
+		_ASSERTEX(getClientRect_f!=NULL);
+	}
+	return lbRc;
+}
+
+HMENU UserImp::getMenu(HWND hWnd)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HMENU hRc = NULL;
+	if (getMenu_f)
+	{
+		hRc = getMenu_f(hWnd);
+	}
+	else
+	{
+		_ASSERTEX(getMenu_f!=NULL);
+	}
+	return hRc;
+}
+
+BOOL UserImp::attachThreadInput(DWORD idAttach, DWORD idAttachTo, BOOL fAttach)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (attachThreadInput_f)
+	{
+		lbRc = attachThreadInput_f(idAttach, idAttachTo, fAttach);
+	}
+	else
+	{
+		_ASSERTEX(attachThreadInput_f!=NULL);
+	}
+	return lbRc;
+}
+
+HWND UserImp::getFocus()
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HWND hRc = NULL;
+	if (getFocus_f)
+	{
+		hRc = getFocus_f();
+	}
+	else
+	{
+		_ASSERTEX(getFocus_f!=NULL);
+	}
+	return hRc;
+}
+
+BOOL UserImp::isWindow(HWND hWnd)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		// Поскольку функция может использоваться при старте (проверка валидности дескриптора ConEmu)
+		// нужно вернуть TRUE, если User32.dll еще не загружен в process space
+		return TRUE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (isWindow_f)
+	{
+		lbRc = isWindow_f(hWnd);
+	}
+	else
+	{
+		_ASSERTEX(isWindow_f!=NULL);
+	}
+	return lbRc;
+}
+
+BOOL UserImp::isWindowVisible(HWND hWnd)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (isWindowVisible_f)
+	{
+		lbRc = isWindowVisible_f(hWnd);
+	}
+	else
+	{
+		_ASSERTEX(isWindowVisible_f!=NULL);
+	}
+	return lbRc;
+}
+
+BOOL UserImp::showWindow(HWND hWnd, int nCmdShow)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+	
+	BOOL lbRc = FALSE;
+	if (showWindow_f)
+	{
+		lbRc = showWindow_f(hWnd, nCmdShow);
+	}
+	else
+	{
+		_ASSERTEX(showWindow_f!=NULL);
+	}
+	return lbRc;
+}
+
+SHORT UserImp::getKeyState(int nVirtKey)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+	
+	SHORT lRc = 0;
+	if (getKeyState_f)
+	{
+		lRc = getKeyState_f(nVirtKey);
+	}
+	else
+	{
+		_ASSERTEX(getKeyState_f!=NULL);
+	}
+	return lRc;
+}
+
+LRESULT UserImp::callNextHookEx(HHOOK hhk, int nCode, WPARAM wParam, LPARAM lParam)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return 0;
+	}
+
+	LRESULT lRc = FALSE;
+	if (callNextHookEx_f)
+	{
+		lRc = callNextHookEx_f(hhk, nCode, wParam, lParam);
+	}
+	else
+	{
+		_ASSERTEX(callNextHookEx_f!=NULL);
+	}
+	return lRc;
+}
+
+HHOOK UserImp::setWindowsHookExW(int idHook, HOOKPROC lpfn, HINSTANCE hMod, DWORD dwThreadId)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return NULL;
+	}
+	
+	HHOOK hRc = NULL;
+	if (setWindowsHookExW_f)
+	{
+		hRc = setWindowsHookExW_f(idHook, lpfn, hMod, dwThreadId);
+	}
+	else
+	{
+		_ASSERTEX(setWindowsHookExW_f!=NULL);
+	}
+	return hRc;
+}
+
+BOOL UserImp::unhookWindowsHookEx(HHOOK hhk)
+{
+	if (!bUserLoaded && !loadExports(bAllowLoadLibrary))
+	{
+		_ASSERTEX(hUser32!=NULL);
+		return FALSE;
+	}
+
+	BOOL lbRc = FALSE;
+	if (unhookWindowsHookEx_f)
+	{
+		lbRc = unhookWindowsHookEx_f(hhk);
+	}
+	else
+	{
+		_ASSERTEX(unhookWindowsHookEx_f!=NULL);
+	}
+	return lbRc;
+}
+
---
+++ src/ConEmuHk/UserImp.h
@@ -0,0 +1,128 @@
+
+/*
+Copyright (c) 2011 Maximus5
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+struct UserImp
+{
+public:
+	BOOL     loadExports(BOOL abAllowLoadLibrary);
+	void     setAllowLoadLibrary();
+
+public:
+	BOOL     allowSetForegroundWindow(DWORD dwProcessId);
+	BOOL     setForegroundWindow(HWND hWnd);
+	HWND     getForegroundWindow();
+	DWORD    getWindowThreadProcessId(HWND hWnd, LPDWORD lpdwProcessId);
+	BOOL     setWindowPos(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx, int cy, UINT uFlags);
+	LONG_PTR getWindowLongPtrW(HWND hWnd, int nIndex);
+	LONG_PTR setWindowLongPtrW(HWND hWnd, int nIndex, LONG_PTR dwNewLong);
+	HWND     getParent(HWND hWnd);
+	HWND     setParent(HWND hWndChild, HWND hWndNewParent);
+	BOOL     getWindowRect(HWND hWnd, LPRECT lpRect);
+	BOOL     systemParametersInfoW(UINT uiAction, UINT uiParam, PVOID pvParam, UINT fWinIni);
+	BOOL     setWindowTextW(HWND hWnd, LPCWSTR lpString);
+	BOOL     endDialog(HWND hDlg, INT_PTR nResult);
+	BOOL     postMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	LRESULT  sendMessageW(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	INT_PTR  dialogBoxIndirectParamW(HINSTANCE hInstance, LPCDLGTEMPLATEW hDialogTemplate, HWND hWndParent, DLGPROC lpDialogFunc, LPARAM dwInitParam);
+	int      getClassNameW(HWND hWnd, LPWSTR lpClassName, int nMaxCount);
+	BOOL     getClientRect(HWND hWnd, LPRECT lpRect);
+	HMENU    getMenu(HWND hWnd);
+	BOOL     attachThreadInput(DWORD idAttach, DWORD idAttachTo, BOOL fAttach);
+	HWND     getFocus();
+	BOOL     isWindow(HWND hWnd);
+	BOOL     isWindowVisible(HWND hWnd);
+	BOOL     showWindow(HWND hWnd, int nCmdShow);
+	SHORT    getKeyState(int nVirtKey);
+	LRESULT  callNextHookEx(HHOOK hhk, int nCode, WPARAM wParam, LPARAM lParam);
+	HHOOK    setWindowsHookExW(int idHook, HOOKPROC lpfn, HINSTANCE hMod, DWORD dwThreadId);
+	BOOL     unhookWindowsHookEx(HHOOK hhk);
+	
+private:
+	HMODULE  hUser32;
+	BOOL     bUserLoaded;
+	BOOL     bAllowLoadLibrary;
+private:
+	typedef BOOL     (WINAPI* allowSetForegroundWindow_t)(DWORD dwProcessId);
+	allowSetForegroundWindow_t allowSetForegroundWindow_f;
+	typedef BOOL     (WINAPI* setForegroundWindow_t)(HWND hWnd);
+	setForegroundWindow_t setForegroundWindow_f;
+	typedef HWND     (WINAPI* getForegroundWindow_t)();
+	getForegroundWindow_t getForegroundWindow_f;
+	typedef DWORD    (WINAPI* getWindowThreadProcessId_t)(HWND hWnd, LPDWORD lpdwProcessId);
+	getWindowThreadProcessId_t getWindowThreadProcessId_f;
+	typedef BOOL     (WINAPI* setWindowPos_t)(HWND hWnd, HWND hWndInsertAfter, int X, int Y, int cx, int cy, UINT uFlags);
+	setWindowPos_t setWindowPos_f;
+	typedef LONG_PTR (WINAPI* getWindowLongPtrW_t)(HWND hWnd, int nIndex);
+	getWindowLongPtrW_t getWindowLongPtrW_f;
+	typedef LONG_PTR (WINAPI* setWindowLongPtrW_t)(HWND hWnd, int nIndex, LONG_PTR dwNewLong);
+	setWindowLongPtrW_t setWindowLongPtrW_f;
+	typedef HWND     (WINAPI* getParent_t)(HWND hWnd);
+	getParent_t getParent_f;
+	typedef HWND     (WINAPI* setParent_t)(HWND hWndChild, HWND hWndNewParent);
+	setParent_t setParent_f;
+	typedef BOOL     (WINAPI* getWindowRect_t)(HWND hWnd, LPRECT lpRect);
+	getWindowRect_t getWindowRect_f;
+	typedef BOOL     (WINAPI* systemParametersInfoW_t)(UINT uiAction, UINT uiParam, PVOID pvParam, UINT fWinIni);
+	systemParametersInfoW_t systemParametersInfoW_f;
+	typedef BOOL     (WINAPI* setWindowTextW_t)(HWND hWnd, LPCWSTR lpString);
+	setWindowTextW_t setWindowTextW_f;
+	typedef BOOL     (WINAPI* endDialog_t)(HWND hDlg, INT_PTR nResult);
+	endDialog_t endDialog_f;
+	typedef BOOL     (WINAPI* postMessageW_t)(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	postMessageW_t postMessageW_f;
+	typedef LRESULT  (WINAPI* sendMessageW_t)(HWND hWnd, UINT Msg, WPARAM wParam, LPARAM lParam);
+	sendMessageW_t sendMessageW_f;
+	typedef INT_PTR  (WINAPI* dialogBoxIndirectParamW_t)(HINSTANCE hInstance, LPCDLGTEMPLATEW hDialogTemplate, HWND hWndParent, DLGPROC lpDialogFunc, LPARAM dwInitParam);
+	dialogBoxIndirectParamW_t dialogBoxIndirectParamW_f;
+	typedef int      (WINAPI* getClassNameW_t)(HWND hWnd, LPWSTR lpClassName, int nMaxCount);
+	getClassNameW_t getClassNameW_f;
+	typedef BOOL     (WINAPI* getClientRect_t)(HWND hWnd, LPRECT lpRect);
+	getClientRect_t getClientRect_f;
+	typedef HMENU    (WINAPI* getMenu_t)(HWND hWnd);
+	getMenu_t getMenu_f;
+	typedef BOOL     (WINAPI* attachThreadInput_t)(DWORD idAttach, DWORD idAttachTo, BOOL fAttach);
+	attachThreadInput_t attachThreadInput_f;
+	typedef HWND     (WINAPI* getFocus_t)();
+	getFocus_t getFocus_f;
+	typedef BOOL     (WINAPI* isWindow_t)(HWND hWnd);
+	isWindow_t isWindow_f;
+	typedef BOOL     (WINAPI* isWindowVisible_t)(HWND hWnd);
+	isWindowVisible_t isWindowVisible_f;
+	typedef BOOL     (WINAPI* showWindow_t)(HWND hWnd, int nCmdShow);
+	showWindow_t showWindow_f;
+	typedef SHORT    (WINAPI* getKeyState_t)(int nVirtKey);
+	getKeyState_t getKeyState_f;
+	typedef LRESULT  (WINAPI* callNextHookEx_t)(HHOOK hhk, int nCode, WPARAM wParam, LPARAM lParam);
+	callNextHookEx_t callNextHookEx_f;
+	typedef HHOOK    (WINAPI* setWindowsHookExW_t)(int idHook, HOOKPROC lpfn, HINSTANCE hMod, DWORD dwThreadId);
+	setWindowsHookExW_t setWindowsHookExW_f;
+	typedef BOOL     (WINAPI* unhookWindowsHookEx_t)(HHOOK hhk);
+	unhookWindowsHookEx_t unhookWindowsHookEx_f;
+};
+
+extern UserImp* user;
