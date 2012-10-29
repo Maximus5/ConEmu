2cfc551fb486fe7f8ed39b1827d53bf0cedc2c2c
ConEmu.Maximus5 <ConEmu.Maximus5@92116c04-ab5e-ca4c-3a8b-6f4eadf4b4a1>
2012-10-29 00:33:39 +0000

    [svn] ConEmu.121028 sources and AutoUpdates information
    git-svn-id: https://conemu-maximus5.googlecode.com/svn@1113 92116c04-ab5e-ca4c-3a8b-6f4eadf4b4a1

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 12,10,22,0
-#define CONEMUVERS "121022"
-#define CONEMUVERL L"121022"
-#define MSI86 "../ConEmu.121022.x86.msi"
-#define MSI64 "../ConEmu.121022.x64.msi"
+#define CONEMUVERN 12,10,28,0
+#define CONEMUVERS "121028"
+#define CONEMUVERL L"121028"
+#define MSI86 "../ConEmu.121028.x86.msi"
+#define MSI64 "../ConEmu.121028.x64.msi"
