3c1b50f618fc62574236df6d163536379a66d830
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-11 00:27:44 +0400

    Polishing. Installer

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,11,5,0
-#define CONEMUVERS "131105"
-#define CONEMUVERL L"131105"
-#define MSI86 "../ConEmu.131105.x86.msi"
-#define MSI64 "../ConEmu.131105.x64.msi"
+#define CONEMUVERN 13,11,7,0
+#define CONEMUVERS "131107"
+#define CONEMUVERL L"131107"
+#define MSI86 "../ConEmu.131107.x86.msi"
+#define MSI64 "../ConEmu.131107.x64.msi"
