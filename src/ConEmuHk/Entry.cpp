4c254255cbe74b4db1bfaf636f3d47cb5fa80279
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-11 13:54:33 +0400

    Make files

--- src/ConEmuHk/Entry.cpp
+++ src/ConEmuHk/Entry.cpp
@@ -31,7 +31,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //	#define SHOW_STARTED_MSGBOX
 //	#define SHOW_INJECT_MSGBOX
 	#define SHOW_EXE_MSGBOX // показать сообщение при загрузке в определенный exe-шник (SHOW_EXE_MSGBOX_NAME)
-	#define SHOW_EXE_MSGBOX_NAME L"xxx.exe"
+	#define SHOW_EXE_MSGBOX_NAME L"vim.exe"
 //	#define SHOW_EXE_TIMINGS
 #endif
 //#define SHOW_INJECT_MSGBOX
