4dcd82c6af023655a9c80cdf1a7fc0787e3e9ca7
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-12 01:33:14 +0400

    Polishing. git structure

--- Release/ConEmu/ConEmu.Addons/FarHereUninstall.cmd
+++
@@ -1,3 +0,0 @@
-@echo off
-
-call "%~dp0FarHereInstall.cmd" /u
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,11,7,0
-#define CONEMUVERS "131107"
-#define CONEMUVERL L"131107"
-#define MSI86 "../ConEmu.131107.x86.msi"
-#define MSI64 "../ConEmu.131107.x64.msi"
+#define CONEMUVERN 13,11,10,1
+#define CONEMUVERS "131110a"
+#define CONEMUVERL L"131110a"
+#define MSI86 "../ConEmu.131110a.x86.msi"
+#define MSI64 "../ConEmu.131110a.x64.msi"
