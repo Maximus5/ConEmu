d15ab6694d9560bde8cf9fc46f0653e9680af7f1
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-15 20:04:01 +0400

    Release 131114

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,11,10,1
-#define CONEMUVERS "131110a"
-#define CONEMUVERL L"131110a"
-#define MSI86 "../ConEmu.131110a.x86.msi"
-#define MSI64 "../ConEmu.131110a.x64.msi"
+#define CONEMUVERN 13,11,14,0
+#define CONEMUVERS "131114"
+#define CONEMUVERL L"131114"
+#define MSI86 "../ConEmu.131114.x86.msi"
+#define MSI64 "../ConEmu.131114.x64.msi"
