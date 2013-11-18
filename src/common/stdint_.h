cb9c0fb0e2cb3d669d466276696c542e398632ed
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-19 01:52:24 +0400

    stdint_.h formatting

--- src/common/stdint_.h
+++ src/common/stdint_.h
@@ -43,9 +43,9 @@ typedef unsigned   uint_least32_t;
 typedef long long  int_least64_t;
 typedef unsigned long long   uint_least64_t;
 
-/*  7.18.1.3  Fastest minimum-width integer types 
+/*  7.18.1.3  Fastest minimum-width integer types
  *  Not actually guaranteed to be fastest for all purposes
- *  Here we use the exact-width types for 8 and 16-bit ints. 
+ *  Here we use the exact-width types for 8 and 16-bit ints.
  */
 typedef signed char int_fast8_t;
 typedef unsigned char uint_fast8_t;
@@ -84,7 +84,7 @@ typedef unsigned long long uintmax_t;
 #if !defined ( __cplusplus) || defined (__STDC_LIMIT_MACROS)
 
 /* 7.18.2.1  Limits of exact-width integer types */
-#define INT8_MIN (-128) 
+#define INT8_MIN (-128)
 #define INT16_MIN (-32768)
 #define INT32_MIN (-2147483647 - 1)
 #define INT64_MIN  (-9223372036854775807LL - 1)
@@ -157,7 +157,7 @@ typedef unsigned long long uintmax_t;
 
 #define SIZE_MAX UINTPTR_MAX
 
-#ifndef WCHAR_MIN  /* also in wchar.h */ 
+#ifndef WCHAR_MIN  /* also in wchar.h */
 #define WCHAR_MIN 0
 #define WCHAR_MAX 0xffff /* UINT16_MAX */
 #endif
