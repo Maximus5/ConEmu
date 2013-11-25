04f9d243a046442d8c7a417dc8d291c41d04b39e
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-26 00:21:28 +0400

    Cleaning VC makefiles

--- src/clean.bat
+++ src/clean.bat
@@ -1,10 +1,15 @@
-@echo off
+@echo on
 
 echo Cleaning obj files ...
 
-if exist "%~dp0_VCBUILD" echo rd /Q /S "%~dp0_VCBUILD" & rd /Q /S "%~dp0_VCBUILD"
+if exist "%~dp0_VCBUILD" (
+echo rd /Q /S "%~dp0_VCBUILD"
+rd /Q /S "%~dp0_VCBUILD"
+)
 
-del /S /Q "%~dp0..\Release\*.lib">nul
-del /S /Q "%~dp0..\Release\*.exp">nul
+del /S /Q "%~dp0..\Release\*.lib"
+del /S /Q "%~dp0..\Release\*.exp"
 
 echo Cleaning obj files done
+
+@echo off
