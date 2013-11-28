8fe415cb08926211d12c520b442868a2e32f20b4
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-11-28 20:39:10 +0400

    131126

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,11,25,0
-#define CONEMUVERS "131125"
-#define CONEMUVERL L"131125"
-#define MSI86 "../ConEmu.131125.x86.msi"
-#define MSI64 "../ConEmu.131125.x64.msi"
+#define CONEMUVERN 13,11,26,0
+#define CONEMUVERS "131126"
+#define CONEMUVERL L"131126"
+#define MSI86 "../ConEmu.131126.x86.msi"
+#define MSI64 "../ConEmu.131126.x64.msi"
