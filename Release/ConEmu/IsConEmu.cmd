42e5dccd87a3061be4cd0941785e4ce0092e7473
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-12-04 20:16:25 +0400

    IsConEmu.cmd updated, returns errorlevel 0,1,2

--- Release/ConEmu/IsConEmu.cmd
+++ Release/ConEmu/IsConEmu.cmd
@@ -1,5 +1,15 @@
 @echo off
 
+rem Usage example for your batch files
+rem   call IsConEmu.cmd > nul
+rem  if errorlevel 1 (
+rem   ... Your batch started out of ConEmu, simple command prompt
+rem  ) else (
+rem   ... Your batch is running in active ConEmu window
+rem  )
+
+
+rem Don't change any env.variables outside of this batch
 setlocal
 
 rem For ANSI example you may set "YES" instead of "NO"
@@ -24,6 +34,7 @@ goto showresult
 ) else (
 
 echo "%~dp0ConEmuC.exe" not found!
+exit /b 2
 goto :EOF
 
 )
@@ -37,6 +48,7 @@ set ESC=
 rem Check if ANSI is enabled
 if not "%ConEmuANSI%"=="ON" (
 echo ConEmu ANSI X3.64 was not enabled!
+exit /b 2
 goto :EOF
 )
 
@@ -45,6 +57,8 @@ echo %ESC%[s%ESC%]9;6;"IsConEmu"%ESC%\%ESC%[u%ESC%[A
 :showresult
 if "%ConEmuMacroResult%"=="Yes" (
 echo ConEmu found!
+exit /b 0
 ) else (
 echo ConEmu NOT found!
+exit /b 1
 )
