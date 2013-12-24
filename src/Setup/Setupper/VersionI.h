88a2edaa50ccbcd6114e0d6e6670af20eb59267b
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-12-24 11:51:08 +0400

    131223 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 13,12,19,0
-#define CONEMUVERS "131219"
-#define CONEMUVERL L"131219"
-#define MSI86 "../ConEmu.131219.x86.msi"
-#define MSI64 "../ConEmu.131219.x64.msi"
+#define CONEMUVERN 13,12,23,0
+#define CONEMUVERS "131223"
+#define CONEMUVERL L"131223"
+#define MSI86 "../ConEmu.131223.x86.msi"
+#define MSI64 "../ConEmu.131223.x64.msi"
