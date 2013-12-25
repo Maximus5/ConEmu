b84ecdc1580111fa8a5ebe14933c7052ae0090df
Maximus5 <ConEmu.Maximus5@gmail.com>
2013-12-26 01:44:35 +0400

    131225 release files

--- Release/UnitTests.cmd
+++ Release/UnitTests.cmd
@@ -26,19 +26,29 @@ goto fin
 call RenameTab "Root"
 call cecho /Green "This is first tab, running new tab with two splits"
 c:\windows\system32\cmd.exe -new_console /k "%~0" tab1
-c:\windows\system32\cmd.exe -new_console:s2TV /k "%~0" tab2
+c:\windows\syswow64\cmd.exe -new_console:s2TV /k "%~0" tab2
+c:\windows\syswow64\cmd.exe -new_console:b /k "%~0" tab3
 goto fin
 
 :tab1
 call RenameTab "Tab1"
-call cecho /Green "This is tab1"
+call cecho /Green "This is tab1 running x64 cmd"
+set Progr
 goto fin
 
 :tab2
 call RenameTab "Tab2"
-call cecho /Yellow "This is tab2"
+call cecho /Yellow "This is tab2 running x32 cmd"
+set Progr
 goto fin
 
+:tab3
+call RenameTab "Ansi"
+type "%ConEmuBaseDir%\Addons\AnsiColors256.ans"
+pause
+333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333 3333333
+pause
+goto fin
 
 :fin
 pause
--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 131223
+// 131225
 #define MVV_1 13
 #define MVV_2 12
-#define MVV_3 23
+#define MVV_3 25
 #define MVV_4 0
 #define MVV_4a ""
 //
