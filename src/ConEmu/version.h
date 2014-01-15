26675815cd6d5db2702b7f1983719e86b29e77dd
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-01-15 20:00:06 +0400

    140114a release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,8 +1,8 @@
-// 140114
+// 140114a
 #define MVV_1 14
 #define MVV_2 1
 #define MVV_3 14
-#define MVV_4 0
-#define MVV_4a ""
+#define MVV_4 1
+#define MVV_4a "a"
 //
 #include "version_macro.h"
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,1,14,0
-#define CONEMUVERS "140114"
-#define CONEMUVERL L"140114"
-#define MSI86 "../ConEmu.140114.x86.msi"
-#define MSI64 "../ConEmu.140114.x64.msi"
+#define CONEMUVERN 14,1,14,1
+#define CONEMUVERS "140114a"
+#define CONEMUVERL L"140114a"
+#define MSI86 "../ConEmu.140114a.x86.msi"
+#define MSI64 "../ConEmu.140114a.x64.msi"
