23cf593655a6b88b1236138c44ae2631059e56b9
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-01-16 20:14:12 +0400

    Internal. _APS_NEXT_CONTROL_VALUE

--- src/ConEmu/resource.h
+++ src/ConEmu/resource.h
@@ -997,7 +997,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        224
 #define _APS_NEXT_COMMAND_VALUE         40010
-#define _APS_NEXT_CONTROL_VALUE         2652
+#define _APS_NEXT_CONTROL_VALUE         2655
 #define _APS_NEXT_SYMED_VALUE           130
 #endif
 #endif
