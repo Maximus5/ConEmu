fe19211455b05495130ed36010b24c85bbe25dc9
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-01-23 02:34:28 +0400

    140122 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,1,17,0
-#define CONEMUVERS "140117"
-#define CONEMUVERL L"140117"
-#define MSI86 "../ConEmu.140117.x86.msi"
-#define MSI64 "../ConEmu.140117.x64.msi"
+#define CONEMUVERN 14,1,22,0
+#define CONEMUVERS "140122"
+#define CONEMUVERL L"140122"
+#define MSI86 "../ConEmu.140122.x86.msi"
+#define MSI64 "../ConEmu.140122.x64.msi"
