32981875074516b1b4ae669c6d37017cc6b7fcd0
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-02-17 03:47:11 +0400

    140216b release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,8 +1,8 @@
-// 140216a
+// 140216b
 #define MVV_1 14
 #define MVV_2 2
 #define MVV_3 16
-#define MVV_4 1
-#define MVV_4a "a"
+#define MVV_4 2
+#define MVV_4a "b"
 //
 #include "version_macro.h"
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,2,16,1
-#define CONEMUVERS "140216a"
-#define CONEMUVERL L"140216a"
-#define MSI86 "../ConEmu.140216a.x86.msi"
-#define MSI64 "../ConEmu.140216a.x64.msi"
+#define CONEMUVERN 14,2,16,2
+#define CONEMUVERS "140216b"
+#define CONEMUVERL L"140216b"
+#define MSI86 "../ConEmu.140216b.x86.msi"
+#define MSI64 "../ConEmu.140216b.x64.msi"
