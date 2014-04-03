41195a39f816f52c1db1a449d5a0f99d68129ae9
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-04-03 20:10:19 +0400

    Add MVV_git to version.h

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,8 +1,9 @@
-// 140327
+// 140401
 #define MVV_1 14
-#define MVV_2 3
-#define MVV_3 27
+#define MVV_2 4
+#define MVV_3 1
 #define MVV_4 0
 #define MVV_4a ""
+#undef MVV_git
 //
 #include "version_macro.h"
