b800a7278ab62a7bc57998c5127b4324353fdca2
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-05-06 00:39:14 +0400

    140505 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,4,29,0
-#define CONEMUVERS "140429"
-#define CONEMUVERL L"140429"
-#define MSI86 "../ConEmu.140429.x86.msi"
-#define MSI64 "../ConEmu.140429.x64.msi"
+#define CONEMUVERN 14,5,5,0
+#define CONEMUVERS "140505"
+#define CONEMUVERL L"140505"
+#define MSI86 "../ConEmu.140505.x86.msi"
+#define MSI64 "../ConEmu.140505.x64.msi"
