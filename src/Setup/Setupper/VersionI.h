a8b3a7aa1a41edd80178807392e74d058d740224
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-07-09 02:25:44 +0400

    140707 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,7,3,0
-#define CONEMUVERS "140703"
-#define CONEMUVERL L"140703"
-#define MSI86 "../ConEmu.140703.x86.msi"
-#define MSI64 "../ConEmu.140703.x64.msi"
+#define CONEMUVERN 14,7,7,0
+#define CONEMUVERS "140707"
+#define CONEMUVERL L"140707"
+#define MSI86 "../ConEmu.140707.x86.msi"
+#define MSI64 "../ConEmu.140707.x64.msi"
