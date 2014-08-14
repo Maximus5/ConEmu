23530cc96d335e23cfe51634d7cd1dbee78db46d
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-08-14 20:01:41 +0400

    Internal. Remove some unused comments

--- src/ConEmu/ConEmuCtrl.cpp
+++ src/ConEmu/ConEmuCtrl.cpp
@@ -1210,8 +1210,6 @@ bool CConEmuCtrl::key_ShowTabsList(DWORD VkMod, bool TestOnly, const ConEmuHotKe
 		if (TestOnly)
 			return true;
 
-		//HMENU hPopup = gpConEmu->CreateVConListPopupMenu(NULL, FALSE);
-
 		RECT rcWnd = {};
 		if (pRCon)
 		{
@@ -1226,62 +1224,7 @@ bool CConEmuCtrl::key_ShowTabsList(DWORD VkMod, bool TestOnly, const ConEmuHotKe
 		POINT pt = {(rcWnd.left+rcWnd.right)/2, (rcWnd.top+rcWnd.bottom)/2};
 
 		ChooseTabFromMenu(FALSE, pt, TPM_CENTERALIGN|TPM_VCENTERALIGN);
-
-		//int nTab = gpConEmu->trackPopupMenu(tmp_TabsList, hPopup, TPM_CENTERALIGN|TPM_VCENTERALIGN|TPM_RETURNCMD,
-		//	x, y, 0, ghWnd, NULL);
-		//if (nTab >= IDM_VCON_FIRST && nTab <= IDM_VCON_LAST)
-		//{
-		//	int nNewV = ((int)HIWORD(nTab))-1;
-		//	int nNewR = ((int)LOWORD(nTab))-1;
-		//	
-		//	CVirtualConsole* pVCon = gpConEmu->GetVCon(nNewV);
-		//	if (pVCon)
-		//	{
-		//		CRealConsole* pRCon = pVCon->RCon();
-		//		if (pRCon)
-		//		{
-		//			pRCon->ActivateFarWindow(nNewR);
-		//		}
-		//		if (!gpConEmu->isActive(pVCon))
-		//			gpConEmu->Activate(pVCon);
-		//	}
-		//}
-
-		//CESERVER_REQ_GETALLTABS::TabInfo* pTabs = NULL;
-		//int Count = (int)CConEmuCtrl::GetOpenedTabs(pTabs);
-		//if ((Count < 1) || !pTabs)
-		//{
-		//	SafeFree(pTabs);
-		//	return false;
-		//}
-
-		//WARNING("должна быть подобная функция для DropDown у тулбара");
-		//HMENU hPopup = CreatePopupMenu();
-
-		//int nLastConsole = 0;
-		//for (int i = 0, k = 0; i < Count; i++, k++)
-		//{
-		//	if (nLastConsole != pTabs[i].ConsoleIdx)
-		//	{
-		//		AppendMenu(hPopup, MIF_SEPARATOR, 0, NULL);
-		//		nLastConsole = pTabs[i].ConsoleIdx;
-		//	}
-		//	DWORD nFlags = 0
-		//		| (pTabs[i].Disabled ? (MF_GRAYED|MF_DISABLED) : MF_ENABLED)
-		//		| 
-		//	pItems[k].Selected = (pOut->GetAllTabs.Tabs[i].ActiveConsole && pOut->GetAllTabs.Tabs[i].ActiveTab);
-		//	pItems[k].Checked = pOut->GetAllTabs.Tabs[i].ActiveTab;
-		//	pItems[k].Disabled = pOut->GetAllTabs.Tabs[i].Disabled;
-		//	pItems[k].MsgText = pOut->GetAllTabs.Tabs[i].Title;
-		//	pItems[k].UserData = i;
-		//}
-		//DestroyMenu(hPopup);
 	}
-
-	//int AllCount = Count + pTabs[Count-1].ConsoleIdx;
-	//if (pItems)
-	//{
-	//}
 
 	return true;
 }
