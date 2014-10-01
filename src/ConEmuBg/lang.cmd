3ad25bd98d278c998b3b5f8ae98fcfb15486d13f
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-10-01 20:07:00 +0400

    Internal. Plugin lng files generation

--- src/ConEmuBg/lang.cmd
+++ src/ConEmuBg/lang.cmd
@@ -2,8 +2,8 @@
 cd /d "%~dp0"
 if not exist ..\..\Release\plugins\ConEmu\Background md ..\..\Release\plugins\ConEmu\Background
 if exist ..\..\Release\plugins\ConEmu\Background\ConEmuBg_*.lng del ..\..\Release\plugins\ConEmu\Background\ConEmuBg_*.lng
-..\lng.generator.exe -nc -i lang.ini -ol Lang.templ
-for %%g in (..\..\Release\plugins\ConEmu\Background\ConEmuBg_*.lng) do \VCProject\Files\Convert\Convert.exe UTF2OEM %%g
+..\Tools\lng.generator.exe -nc -i lang.ini -ol Lang.templ
+for %%g in (..\..\Release\plugins\ConEmu\Background\ConEmuBg_*.lng) do ..\Tools\Convert.exe UTF2OEM %%g
 if NOT "%~1"=="" (
   echo Copying *.lng files to %1
   copy ..\..\Release\plugins\ConEmu\Background\ConEmuBg_*.lng "%~1"
--- src/ConEmuLn/lang.cmd
+++ src/ConEmuLn/lang.cmd
@@ -2,8 +2,8 @@
 cd /d "%~dp0"
 if not exist ..\..\Release\plugins\ConEmu\Lines md ..\..\Release\plugins\ConEmu\Lines
 if exist ..\..\Release\plugins\ConEmu\Lines\ConEmuLn_*.lng del ..\..\Release\plugins\ConEmu\Lines\ConEmuLn_*.lng
-..\lng.generator.exe -nc -i lang.ini -ol Lang.templ
-for %%g in (..\..\Release\plugins\ConEmu\Lines\ConEmuLn_*.lng) do \VCProject\Files\Convert\Convert.exe UTF2OEM %%g
+..\Tools\lng.generator.exe -nc -i lang.ini -ol Lang.templ
+for %%g in (..\..\Release\plugins\ConEmu\Lines\ConEmuLn_*.lng) do ..\Tools\Convert.exe UTF2OEM %%g
 if NOT "%~1"=="" (
   echo Copying *.lng files to %1
   copy ..\..\Release\plugins\ConEmu\Lines\ConEmuLn_*.lng "%~1"
