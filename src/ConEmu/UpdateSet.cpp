73f35930569066f710d76e2fbd6113534e964b3f
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-11-04 06:10:19 +0300

    Change autoupdate source information to github.

--- src/ConEmu/UpdateSet.cpp
+++ src/ConEmu/UpdateSet.cpp
@@ -49,7 +49,8 @@ LPCWSTR ConEmuUpdateSettings::UpdateVerLocationDefault()
 	static LPCWSTR pszDefault =
 		//L"file://C:\\ConEmu-Update\\version.ini"
 		//L"http://conemu.ru/version.ini"
-		L"http://conemu-maximus5.googlecode.com/svn/trunk/ConEmu/version.ini"
+		//L"http://conemu-maximus5.googlecode.com/svn/trunk/ConEmu/version.ini"
+		L"http://conemu.github.io/version.ini"
 		;
 	return pszDefault;
 }
