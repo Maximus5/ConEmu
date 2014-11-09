bcfb4980257e9683604333b0af2820c1c35b88c8
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-11-09 23:30:49 +0300

    141109 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 141107
+// 141109
 #define MVV_1 14
 #define MVV_2 11
-#define MVV_3 7
+#define MVV_3 9
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 14,11,7,0
-#define CONEMUVERS "141107"
-#define CONEMUVERL L"141107"
-#define MSI86 "../ConEmu.141107.x86.msi"
-#define MSI64 "../ConEmu.141107.x64.msi"
+#define CONEMUVERN 14,11,9,0
+#define CONEMUVERS "141109"
+#define CONEMUVERL L"141109"
+#define MSI86 "../ConEmu.141109.x86.msi"
+#define MSI64 "../ConEmu.141109.x64.msi"
