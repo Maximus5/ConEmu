3d79f08a90a1904897dee906637e5b7197d743cd
Maximus5 <ConEmu.Maximus5@gmail.com>
2014-11-14 20:22:12 +0300

    Add 'CmdInit.cmd' to `ConEmuBaseDir`.

---
+++ Release/ConEmu/CmdInit.cmd
@@ -0,0 +1,39 @@
+@rem !!! Do not change this file in-place, change its copy instead !!!
+@rem !!!  Otherwise you will lose your settings after next update  !!!
+
+@echo off
+
+rem This will start prompt with `User@PC `
+set ConEmuPrompt0=$E[32m$E]9;8;"USERNAME"$E\@$E]9;8;"COMPUTERNAME"$E\$S
+
+rem Followed by colored `Path`
+set ConEmuPrompt1=%ConEmuPrompt0%$E[92m$P$E[90m
+if NOT "%PROCESSOR_ARCHITECTURE%" == "AMD64" (
+  if "%PROCESSOR_ARCHITEW6432%" == "AMD64" if "%PROCESSOR_ARCHITECTURE%" == "x86" (
+    rem Use another text color if cmd was run from SysWow64
+    set ConEmuPrompt1=%ConEmuPrompt0%$E[93m$P$E[90m
+  )
+)
+
+rem Carriage return and `$` or `>`
+rem Spare `$E[90m` was specially added because of GitShowBranch.cmd
+if "%ConEmuIsAdmin%" == "ADMIN" (
+  set ConEmuPrompt2=$_$E[90m$$
+) else (
+  set ConEmuPrompt2=$_$E[90m$G
+)
+
+rem Finally reset color and add space
+set ConEmuPrompt3=$E[m$S
+
+if /I "%~1" == "/git" goto git
+if /I "%~1" == "-git" goto git
+goto no_git
+
+:git
+call "%~dp0GitShowBranch.cmd" /i
+goto :EOF
+
+:no_git
+rem Set new prompt
+PROMPT %ConEmuPrompt1%%ConEmuPrompt2%%ConEmuPrompt3%
