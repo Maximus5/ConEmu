48a53a43e57fcf7361f764b179cf754e48554e71
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-02-09 20:17:25 +0300

    Add test batch 'utf-8-test.cmd' (check console unicode capabilities).

---
+++ Release/ConEmu/Addons/utf-8-test.cmd
@@ -0,0 +1,22 @@
+@chcp 65001 & (cmd /c type "%~dpn0.cmd") & pause & goto :EOF
+
+Specials:    ← → ↔ ↑ ↓ ↕ ⇔ ⇐ ⇑ ⇒ ⇓
+Ligatures:   ffttft ==> ==< === == (unsupported yet)
+English:     texts, web pages and documents
+Graves,etc:  à á â ã ä å æ ç è é ê ë ì í î ï
+Greek:       ΐ Α Β Γ Δ Ε Ζ Η Θ Ι Κ Λ Μ Ν Ξ Ο
+Arabic:      ڠ ڡ ڢ ڣ ڤ ڥ ڦ ڧ ڨ ک ڪ ګ ڬ ڭ ڮ گ
+Full width:  ＠ Ａ Ｂ Ｃ Ｄ Ｅ Ｆ Ｇ Ｈ Ｉ Ｊ Ｋ Ｌ Ｍ Ｎ Ｏ
+Romanian:    texte, pagini Web şi a documentelor
+Vietnamese:  văn bản, các trang web và các tài liệu
+Russian:     тексты, веб-страницы и документы
+Japanese:    テキスト、Webページや文書
+Yiddish:     טעקסץ, וועב זייַטלעך און דאָקומענטן
+Hindi:       पाठ, वेब पृष्ठों और दस्तावेज
+Thai:        ข้อความ หน้า เว็บ และ เอกสาร
+Korean:      텍스트, 웹 페이지 및 문서
+Chinese:     文本，網頁和文件
+
+If you can read the output of this batch
+your console runs correctly.
+
