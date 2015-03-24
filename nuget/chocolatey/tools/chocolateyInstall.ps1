db09c1c2384e43e4cff64ea22787841a9f592d66
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-03-24 20:33:23 +0300

    Simplify chocolatey scripts

--- nuget/chocolatey/tools/chocolateyInstall.ps1
+++ nuget/chocolatey/tools/chocolateyInstall.ps1
@@ -1,52 +1,24 @@
 $package = 'ConEmu'
 $version = '15.03.16'
 
-try {
 
-  $isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
-    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+$isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
+  ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
 
-  $os = if ($isSytem32Bit) { "x86" } else { "x64" }
+$os = if ($isSytem32Bit) { "x86" } else { "x64" }
 
-  $url = "https://downloads.sf.net/project/conemu/Alpha/ConEmuSetup.$($version.replace('.','')).exe?use_mirror=autoselect"
-  #$url = "https://github.com/Maximus5/ConEmu/releases/download/v$version/ConEmuSetup.$($version.replace('.','')).exe"
-  #$url = "file:///C:\ConEmu-Deploy\ConEmuSetup.$($version.replace('.','')).exe"
+$url = "https://downloads.sf.net/project/conemu/Alpha/ConEmuSetup.$($version.replace('.','')).exe?use_mirror=autoselect"
+#$url = "https://github.com/Maximus5/ConEmu/releases/download/v$version/ConEmuSetup.$($version.replace('.','')).exe"
+#$url = "file:///C:\ConEmu-Deploy\ConEmuSetup.$($version.replace('.','')).exe"
 
-  $chocTemp = Join-Path $Env:TEMP 'chocolatey'
-  $tempFolder = Join-Path $chocTemp "ConEmu"
-  $tempInstall = Join-Path $tempFolder "ConEmuSetup.$version.exe"
-
-  # Check if our temp folder exists
-  if ([System.IO.Directory]::Exists($tempFolder)) {
-    Write-Host "Temp directory '$tempFolder' exists"
-  } else {
-    Write-Host "Creating temp directory '$tempFolder'"
-    New-Item $tempFolder -itemtype directory
-  }
-
-  # Retrieve installer from server
-  Write-Host "Downloading from $url to $tempInstall"
-  $params = @{
-    PackageName = $package;
-    fileFullPath = $tempInstall;
-    url = $url;
-  }
-  Get-ChocolateyWebFile @params
-  Write-Host "Download from $url complete"
-
-  # MSI installer, but packed inside wrapper to select x86 or x64
-  # version. Therefore, treat it as EXE type.
-  $params = @{
-    PackageName = $package;
-    FileType = 'exe';
-    SilentArgs = "/p:$os /passive";
-    File = $tempInstall;
-  }
-  Install-ChocolateyInstallPackage @params
-
-  # Done
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+# MSI installer, but packed inside wrapper to select x86 or x64
+# version. Therefore, treat it as EXE type.
+$params = @{
+  PackageName = $package;
+  FileType = 'exe';
+  SilentArgs = "/p:$os /passive";
+  File = $tempInstall;
 }
+Install-ChocolateyPackage @params
+
+# Done
--- nuget/chocolatey/tools/chocolateyUninstall.ps1
+++ nuget/chocolatey/tools/chocolateyUninstall.ps1
@@ -1,37 +1,32 @@
 $package = 'ConEmu'
 $version = '15.03.16'
 
-try {
 
-  $isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
-    ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
+$isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
+  ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
 
-  $os = if ($isSytem32Bit) { "x86" } else { "x64" }
+$os = if ($isSytem32Bit) { "x86" } else { "x64" }
 
-  $displayName = "ConEmu $($version.replace('.','')).$os"
+$displayName = "ConEmu $($version.replace('.','')).$os"
 
-  $installerRoot = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer'
-  $productsRoot = "$installerRoot\UserData\S-1-5-18\Products"
+$installerRoot = 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer'
+$productsRoot = "$installerRoot\UserData\S-1-5-18\Products"
 
-  Write-Host "Searching for installed msi-packet..."
+Write-Host "Searching for installed msi-packet..."
 
-  # ConEmu packages do not have fixed GUIDs
-  $pkg = ""
-  Get-ChildItem $productsRoot |
-    % { ($productsRoot + "\" + (Split-Path -Leaf $_.Name) + "\InstallProperties") } |
-    ? { ($pkg -eq "") -And (Test-Path $_) } |
-    ? { ((Get-ItemProperty $_).DisplayName -eq $displayName) } |
-    % {
-      $pkg = (Get-ItemProperty $_).LocalPackage
-    }
-  if ($pkg -ne "") {
-    # Write-Host "Found at $pkg"
-    Write-Host "Uninstalling '$displayName' via 'msiexec /x $pkg'"
-    Start-Process -Wait msiexec -ArgumentList @("/x", $pkg, "/qb-!")
+# ConEmu packages do not have fixed GUIDs
+$pkg = ""
+Get-ChildItem $productsRoot |
+  % { ($productsRoot + "\" + (Split-Path -Leaf $_.Name) + "\InstallProperties") } |
+  ? { ($pkg -eq "") -And (Test-Path $_) } |
+  ? { ((Get-ItemProperty $_).DisplayName -eq $displayName) } |
+  % {
+    $pkg = (Get-ItemProperty $_).LocalPackage
   }
-
-  Write-ChocolateySuccess $package
-} catch {
-  Write-ChocolateyFailure $package "$($_.Exception.Message)"
-  throw
+if ($pkg -ne "") {
+  # Write-Host "Found at $pkg"
+  Write-Host "Uninstalling '$displayName' via 'msiexec /x $pkg'"
+  Start-Process -Wait msiexec -ArgumentList @("/x", $pkg, "/qb-!")
 }
+
+# Done
