4faa066f06bda438594fd8a9f23d0084501ecddd
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-03-24 20:35:53 +0300

    Add successful chocolatey note

--- nuget/build.cmd
+++ nuget/build.cmd
@@ -13,6 +13,8 @@ if errorlevel 1 (
   call cecho "Creation Chocolatey package was failed"
   exit /b 100
 )
+call cecho /green "Chocolatey package was created"
+
 move "%~dp0chocolatey\ConEmu.*.nupkg" "%~dp0"
 if errorlevel 1 (
   call cecho "Moving Chocolatey package was failed"
