486c8bee612eb7b95d54c63da5b613b74c3b81c1
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-06-02 20:07:36 +0300

    Internal. Wrong ID for vkCdExplorerPath was used

--- src/ConEmu/resource.h
+++ src/ConEmu/resource.h
@@ -1168,7 +1168,7 @@
 #define gbSetAlias                      2850
 #define tSetCommands                    2851
 #define stSetCommands                   2852
-#define vkCdExplorerPath                3841
+#define vkCdExplorerPath                2853
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -1177,7 +1177,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        227
 #define _APS_NEXT_COMMAND_VALUE         40010
-#define _APS_NEXT_CONTROL_VALUE         2853
+#define _APS_NEXT_CONTROL_VALUE         2854
 #define _APS_NEXT_SYMED_VALUE           130
 #endif
 #endif
