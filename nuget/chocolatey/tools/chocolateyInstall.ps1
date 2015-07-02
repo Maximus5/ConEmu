1ebe1ecfd5085dc28fb6cc6f439775474fc81618
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-07-02 11:34:42 +0300

    Redirect chocolatey downloads to github.

--- nuget/chocolatey/tools/chocolateyInstall.ps1
+++ nuget/chocolatey/tools/chocolateyInstall.ps1
@@ -1,14 +1,12 @@
 $package = 'ConEmu'
 $version = '15.06.29'
-$stage   = 'Preview'
-
 
 $isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
   ($Env:PROCESSOR_ARCHITEW6432 -eq $null))
 
 $os = if ($isSytem32Bit) { "x86" } else { "x64" }
 
-$url = "https://downloads.sf.net/project/conemu/$stage/ConEmuSetup.$($version.replace('.','')).exe?use_mirror=autoselect"
+$url = "https://github.com/Maximus5/ConEmu/releases/download/v$version/ConEmuSetup.$($version.replace('.','')).exe"
 
 # MSI installer, but packed inside wrapper to select x86 or x64
 # version. Therefore, treat it as EXE type.
