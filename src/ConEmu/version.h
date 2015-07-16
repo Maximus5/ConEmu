ec4efe6da540b01602e4a6f80f5165b01448782f
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-07-17 01:38:46 +0300

    150716 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 150707
+// 150716
 #define MVV_1 15
 #define MVV_2 7
-#define MVV_3 7
+#define MVV_3 16
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 15,7,7,0
-#define CONEMUVERS "150707"
-#define CONEMUVERL L"150707"
-#define MSI86 "../ConEmu.150707.x86.msi"
-#define MSI64 "../ConEmu.150707.x64.msi"
+#define CONEMUVERN 15,7,16,0
+#define CONEMUVERS "150716"
+#define CONEMUVERL L"150716"
+#define MSI86 "../ConEmu.150716.x86.msi"
+#define MSI64 "../ConEmu.150716.x64.msi"
