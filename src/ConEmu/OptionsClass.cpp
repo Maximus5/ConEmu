e3d1d72c7d94f3305bccc3ce95eccd54170409eb
Michael Lukashov <michael.lukashov@gmail.com>
2015-08-08 13:38:43 +0300

    Fix Coverity warning: Same on both sides / CSettings::SetOption
    This is often the result of a copy and paste error. It may result in the operator always evaluating to the same result.

--- src/ConEmu/OptionsClass.cpp
+++ src/ConEmu/OptionsClass.cpp
@@ -707,7 +707,7 @@ bool CSettings::SetOption(LPCWSTR asName, LPCWSTR asValue)
 	if (!lstrcmpi(asName, L"FarGotoEditorPath"))
 	{
 		wchar_t* pszNewVal = lstrdup(asValue);
-		if (pszNewVal && pszNewVal)
+		if (pszNewVal && *pszNewVal)
 		{
 			wchar_t* pszOld = (wchar_t*)InterlockedExchangePointer((PVOID*)&gpSet->sFarGotoEditor, pszNewVal);
 			SafeFree(pszOld);
