2c450b257e1954140ac7ba08c624fd39c8e84585
Michael Lukashov <michael.lukashov@gmail.com>
2015-08-08 15:05:42 +0300

    Fix memory leak

--- src/ConEmu/OptionsClass.cpp
+++ src/ConEmu/OptionsClass.cpp
@@ -4945,6 +4945,7 @@ LRESULT CSettings::OnEditChanged(HWND hWnd2, WPARAM wParam, LPARAM lParam)
 
 	case tTabSkipWords:
 	{
+		SafeFree(gpSet->pszTabSkipWords);
 		gpSet->pszTabSkipWords = GetDlgItemTextPtr(hWnd2, TB);
 		gpConEmu->mp_TabBar->Update(TRUE);
 		break;
