2e903e565e6e63384dbfbb9024475a3c07c2c007
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-11-27 13:02:02 +0300

    151126 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 15,11,22,0
-#define CONEMUVERS "151122"
-#define CONEMUVERL L"151122"
-#define MSI86 "../ConEmu.151122.x86.msi"
-#define MSI64 "../ConEmu.151122.x64.msi"
+#define CONEMUVERN 15,11,26,0
+#define CONEMUVERS "151126"
+#define CONEMUVERL L"151126"
+#define MSI86 "../ConEmu.151126.x86.msi"
+#define MSI64 "../ConEmu.151126.x64.msi"
