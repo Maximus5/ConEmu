249c4c739f099bf136d5dd2328cb8605b6996f54
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-11-30 03:55:34 +0300

    151129 release files

--- nuget/chocolatey/tools/chocolateyUninstall.ps1
+++ nuget/chocolatey/tools/chocolateyUninstall.ps1
@@ -1,5 +1,5 @@
 $package = 'ConEmu'
-$version = '15.11.26'
+$version = '15.11.29'
 
 
 $isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 151126
+// 151129
 #define MVV_1 15
 #define MVV_2 11
-#define MVV_3 26
+#define MVV_3 29
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
