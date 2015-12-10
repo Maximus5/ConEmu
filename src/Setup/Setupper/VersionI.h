795d083a92103b82b579ffd0caa1c5f1e1996aab
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-10 23:41:32 +0300

    150813d release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 15,8,13,3
-#define CONEMUVERS "150813c"
-#define CONEMUVERL L"150813c"
-#define MSI86 "../ConEmu.150813c.x86.msi"
-#define MSI64 "../ConEmu.150813c.x64.msi"
+#define CONEMUVERN 15,8,13,4
+#define CONEMUVERS "150813d"
+#define CONEMUVERL L"150813d"
+#define MSI86 "../ConEmu.150813d.x86.msi"
+#define MSI64 "../ConEmu.150813d.x64.msi"
