5ece248f8599ada0bd9124b353e7f99cc0c3e911
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-10 12:48:14 +0300

    Internal. Drop unused includes

--- src/ConEmuPlugin/ConEmuPlugin.cpp
+++ src/ConEmuPlugin/ConEmuPlugin.cpp
@@ -66,13 +66,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "PluginBackground.h"
 #include <Tlhelp32.h>
 
-#ifndef __GNUC__
-	#pragma warning(push)
-	#pragma warning(disable : 4091)
-	#include <Dbghelp.h>
-	#pragma warning(pop)
-#endif
-
 #include "../common/ConEmuCheck.h"
 #include "PluginSrv.h"
 
--- src/ConEmuPlugin/PluginSrv.cpp
+++ src/ConEmuPlugin/PluginSrv.cpp
@@ -56,13 +56,6 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "PluginBackground.h"
 #include <Tlhelp32.h>
 
-#ifndef __GNUC__
-	#pragma warning(push)
-	#pragma warning(disable : 4091)
-	#include <Dbghelp.h>
-	#pragma warning(pop)
-#endif
-
 #include "../common/ConEmuCheck.h"
 #include "../common/PipeServer.h"
 
