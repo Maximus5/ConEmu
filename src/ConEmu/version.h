cd8092a0ebd3543036ec7ce4765c4912b4c93685
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-10 20:39:38 +0300

    150813d

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,9 +1,9 @@
-// 150813c
+// 150813d
 #define MVV_1 15
 #define MVV_2 8
 #define MVV_3 13
-#define MVV_4 3
-#define MVV_4a "c"
+#define MVV_4 4
+#define MVV_4a "d"
 #undef MVV_git
 //
 #include "version_macro.h"
