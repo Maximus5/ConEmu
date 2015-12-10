f5d69aee02656d927bd2009d50becfd3231ee93f
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-10 12:33:37 +0300

    Internal. Move SEH-related code to WModuleCheck.cpp

--- src/common/WObjects.h
+++ src/common/WObjects.h
@@ -84,12 +84,6 @@ typedef struct tagPROCESSENTRY32W PROCESSENTRY32W;
 int  GetProcessBits(DWORD nPID, HANDLE hProcess = NULL);
 bool GetProcessInfo(DWORD nPID, PROCESSENTRY32W* Info);
 
-bool CheckCallbackPtr(HMODULE hModule, size_t ProcCount, FARPROC* CallBack, BOOL abCheckModuleInfo, BOOL abAllowNTDLL = FALSE, BOOL abTestVirtual = TRUE);
-bool IsModuleValid(HMODULE module, BOOL abTestVirtual = TRUE);
-
-typedef LPVOID(WINAPI* VirtualAlloc_t)(LPVOID lpAddress, SIZE_T dwSize, DWORD flAllocationType, DWORD flProtect);
-extern VirtualAlloc_t gfVirtualAlloc;
-
 wchar_t* ExpandMacroValues(LPCWSTR pszFormat, LPCWSTR* pszValues, size_t nValCount);
 wchar_t* ExpandEnvStr(LPCWSTR pszCommand);
 
