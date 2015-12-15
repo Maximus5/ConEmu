64c4c6e1527ab7c736a70753b953d9a75beb5740
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-16 01:42:31 +0300

    Internal. If root process is `ConEmuC` and it starts smth with `-async` console closes.
      Server is not able to find new console process before root terminates,
      that's why there is a "Root process was alive less than 10 sec"...

--- Release/UnitTests.cmd
+++ Release/UnitTests.cmd
@@ -32,7 +32,7 @@ c:\windows\system32\cmd.exe -new_console /k "%~0" tab1
 c:\windows\syswow64\cmd.exe -new_console:s2TV /k "%~0" tab2
 c:\windows\syswow64\cmd.exe -new_console:b /k "%~0" tab3
 c:\windows\syswow64\cmd.exe -new_console:ba /k "%~0" tab4
-c:\windows\syswow64\cmd.exe -new_console:bnt:"Far":C:"%ConEmuDrive%\Far30.Latest\far-bis\Far.exe" /c "%~0" tab5
+c:\windows\syswow64\cmd.exe -new_console:bt:"Far":C:"%ConEmuDrive%\Far30.Latest\far-bis\Far.exe" /c "%~0" tab5
 c:\windows\syswow64\cmd.exe -new_console:abP:"<Monokai>":t:Tab6 /k "%~0" tab6
 c:\windows\syswow64\cmd.exe -new_console:b:t:Tab7 /k "%~0" tab7
 goto fin
@@ -76,7 +76,7 @@ echo Errorlevel=%errorlevel%
 goto fin
 
 :tab5
-conemuc /async /c %ConEmuDrive%\Far30.Latest\far-bis\Far.exe /w /x /p%ConEmuDir%\Plugins\ConEmu;%ConEmuDrive%\Far30.Latest\far-bis\Plugins;%ConEmuDrive%\Far30.Latest\Plugins;%ConEmuDrive%\Far30.Latest\Plugins.My
+conemuc /async /c %ConEmuDrive%\Far30.Latest\far-bis\Far.exe /w- /x /p%ConEmuDir%\Plugins\ConEmu;%ConEmuDrive%\Far30.Latest\far-bis\Plugins;%ConEmuDrive%\Far30.Latest\Plugins;%ConEmuDrive%\Far30.Latest\Plugins.My
 goto :EOF
 
 :tab6
