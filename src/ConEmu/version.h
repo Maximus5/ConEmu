4f210752e0e71d8aa3e134e7798d67e49e51783c
Maximus5 <ConEmu.Maximus5@gmail.com>
2015-12-16 01:46:34 +0300

    150813f release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,9 +1,9 @@
-// 150813e
+// 150813f
 #define MVV_1 15
 #define MVV_2 8
 #define MVV_3 13
-#define MVV_4 5
-#define MVV_4a "e"
+#define MVV_4 6
+#define MVV_4a "f"
 #undef MVV_git
 //
 #include "version_macro.h"
