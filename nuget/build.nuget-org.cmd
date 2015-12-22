d2dea550ae1238c62028cdd60978fd14224eecf1
hypersw <baltic@hypersw.net>
2015-12-22 04:34:13 +0300

    Making package ID consistent: use ConEmu.Console everywhere.

--- nuget/build.nuget-org.cmd
+++ nuget/build.nuget-org.cmd
@@ -5,10 +5,10 @@ setlocal
 set PATH=%~d0\Nuget\bin;%PATH%                           
 rem ??? what would be the path to Nuget.exe cmdline tool?..
 
-if exist "ConEmu.CommandLine.*.nupkg" del "ConEmu.CommandLine.*.nupkg"
-if exist "ConEmu.CommandLine\*.nupkg" del "ConEmu.CommandLine\*.nupkg"
+if exist "ConEmu.Console.*.nupkg" del "ConEmu.Console.*.nupkg"
+if exist "ConEmu.Console\*.nupkg" del "ConEmu.Console\*.nupkg"
 
-cd /d ConEmu.CommandLine
+cd /d ConEmu.Console
 call nuget pack -Verbosity detailed -NoDefaultExcludes
 if errorlevel 1 (
   call cecho "Failed to create the package for Nuget.Org."
@@ -16,8 +16,8 @@ if errorlevel 1 (
 )
 call cecho /green "A package for Nuget.Org were created."
 
-move "%~dp0ConEmu.CommandLine\ConEmu.Console.*.nupkg" "%~dp0"
+move "%~dp0ConEmu.Console\ConEmu.Console.*.nupkg" "%~dp0"
 if errorlevel 1 (
-  call cecho "Moving ConEmu.CommandLine package failed."
+  call cecho "Moving ConEmu.Console package failed."
   exit /b 100
 )
