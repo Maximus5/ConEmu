fb733e3388ee16116348f96cb38fa95986daa47a
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-02-03 04:31:05 +0300

    160202 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 16,1,24,0
-#define CONEMUVERS "160124"
-#define CONEMUVERL L"160124"
-#define MSI86 "../ConEmu.160124.x86.msi"
-#define MSI64 "../ConEmu.160124.x64.msi"
+#define CONEMUVERN 16,2,2,0
+#define CONEMUVERS "160202"
+#define CONEMUVERL L"160202"
+#define MSI86 "../ConEmu.160202.x86.msi"
+#define MSI64 "../ConEmu.160202.x64.msi"
