74300635ecfd9e494022c514e31062bca8ffd585
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-02-05 00:31:04 +0300

    Internal. Allow to dump commits from HEAD to specified sha

--- Deploy/git2log.cmd
+++ Deploy/git2log.cmd
@@ -1,5 +1,8 @@
 @echo off
 
+rem Usage: git2log [<commit-sha>]
+rem If <commit-sha> is specified, script will process range: HEAD...<commit>
+
 rem === Far menu ===
 rem git2log.cmd
 rem edit:...\_posts\.daily.md
@@ -28,7 +31,7 @@ rem Create YAML header
 :yaml
 call :out_build > %daily_md%
 rem Dump git commits into ".daily.md"
-call :do_log >> %daily_md%
+call :do_log %1 >> %daily_md%
 call cecho /green "%daily_md%"
 rem Done
 :done
@@ -45,12 +48,21 @@ goto :EOF
 
 :do_log
 
-set commit_range=daily...origin/master
-
 rem let gitlogpath be folder to store git output
 if "%TEMP:~-1%" == "\" (set "gitlogpath=%TEMP:~0,-1%") else (set "gitlogpath=%TEMP%")
 set git_out=%gitlogpath%\conemu_git_%ConEmuServerPID%_1.log
 set git_err=%gitlogpath%\conemu_git_%ConEmuServerPID%_2.log
+
+if "%~1" NEQ "" (
+  call "%git%" log "%~1" -1 --oneline 1>"%git_out%" 2>"%git_err%"
+  if errorlevel 1 (
+    call cecho "Invalid commit-sha specified: %~1"
+    exit /B 1
+  )
+  set commit_range=HEAD...%~1
+  goto commit_sha
+)
+
 "%git%" -c color.status=false status --short --branch --porcelain 1>"%git_out%" 2>"%git_err%"
 "%WINDIR%\system32\find.exe" "## preview" "%git_out%" > nul
 if errorlevel 1 (
@@ -58,6 +70,7 @@ if errorlevel 1 (
 ) else (
   set commit_range=preview...v-preview
 )
+:commit_sha
 if exist "%git_out%" del "%git_out%"
 if exist "%git_err%" del "%git_err%"
 
