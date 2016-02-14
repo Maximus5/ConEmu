efcaa1ee3447643689b436a8ec8fb58d3353b373
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-02-14 14:55:59 +0300

    Internal. Include CINJECTHK_EXIT_CODES definition

--- src/ConEmuHk/Injects.h
+++ src/ConEmuHk/Injects.h
@@ -4,6 +4,8 @@
 extern UINT_PTR gfnLoadLibrary;
 extern UINT_PTR gfnLdrGetDllHandleByName;
 
+#include "../ConEmuCD/ExitCodes.h"
+
 CINJECTHK_EXIT_CODES InjectHooks(PROCESS_INFORMATION pi, BOOL abLogProcess);
 UINT_PTR GetLoadLibraryAddress();
 UINT_PTR GetLdrGetDllHandleByNameAddress();
