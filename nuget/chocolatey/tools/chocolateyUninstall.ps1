d35d428f28f479293787db66ef9ba509eae2467e
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-02-22 19:04:52 +0300

    160222 release files

--- nuget/chocolatey/tools/chocolateyUninstall.ps1
+++ nuget/chocolatey/tools/chocolateyUninstall.ps1
@@ -1,5 +1,5 @@
 $package = 'ConEmu'
-$version = '16.02.19'
+$version = '16.02.22'
 
 
 $isSytem32Bit = (($Env:PROCESSOR_ARCHITECTURE -eq 'x86') -and `
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 16,2,19,0
-#define CONEMUVERS "160219"
-#define CONEMUVERL L"160219"
-#define MSI86 "../ConEmu.160219.x86.msi"
-#define MSI64 "../ConEmu.160219.x64.msi"
+#define CONEMUVERN 16,2,22,0
+#define CONEMUVERS "160222"
+#define CONEMUVERL L"160222"
+#define MSI86 "../ConEmu.160222.x86.msi"
+#define MSI64 "../ConEmu.160222.x64.msi"
