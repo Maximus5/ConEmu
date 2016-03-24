c4bc39a094af7bbb1ea95b037aa1f6e014e3664b
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-03-24 11:59:01 +0300

    Internal. str-add without args starts in a split

--- src/ConEmu/str-add.cmd
+++ src/ConEmu/str-add.cmd
@@ -2,13 +2,16 @@
 
 setlocal
 
+set new_con=
+
 rem Failed to pass complex strings as ps1 argument
 rem So, we use env var instead...
 if "%~1" == "" (
   set "ce_add_id=-"
+  set new_con=-new_console:s30V
 ) else (
   set "ce_add_id=%~1"
 )
 set "ce_add_str=%~2"
 
-powershell -noprofile -command "%~dp0..\..\Deploy\rc2json.ps1" -mode add -id "%ce_add_id%" %3
+powershell -noprofile -command "%~dp0..\..\Deploy\rc2json.ps1" -mode add -id "%ce_add_id%" %3 %new_con%
