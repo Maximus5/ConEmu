9f539616f730b41c19b341bd39053ffc9416c8d6
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-04-04 20:03:05 +0300

    Internal. Define `u32` as `unsigned __int32`

--- src/common/kl_parts.h
+++ src/common/kl_parts.h
@@ -20,7 +20,7 @@ typedef __int32 i32;
 typedef __int64 i64;
 typedef unsigned __int8 u8;
 typedef unsigned __int16 u16;
-typedef DWORD u32;
+typedef unsigned __int32 u32;
 typedef unsigned __int64 u64;
 typedef INT_PTR ssize_t;
 
