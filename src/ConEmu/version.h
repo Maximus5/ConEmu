783ea844bcf12784c5683f5e2ead0dbaae665e62
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-06-21 20:07:15 +0300

    [in-progress] even closer

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,9 +1,9 @@
-// 160612
+// 160612a
 #define MVV_1 16
 #define MVV_2 6
 #define MVV_3 12
-#define MVV_4 0
-#define MVV_4a ""
+#define MVV_4 1
+#define MVV_4a "a"
 #undef MVV_git
 
 #include "version_stage.h"
