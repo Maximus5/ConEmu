4609a3cfd6617a6649feb6fa4cc10af9c5318e1e
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-06-21 20:02:28 +0300

    Internal. Suppress dbg cleaning errors

--- src/clean_dbg.cmd
+++ src/clean_dbg.cmd
@@ -2,7 +2,7 @@
 
 echo Cleaning dbg files ...
 
-del /S /Q "%~dp0..\Release\*.map" > nul
-del /S /Q "%~dp0..\Release\*.pdb" > nul
+del /S /Q "%~dp0..\Release\*.map" > nul 2> nul
+del /S /Q "%~dp0..\Release\*.pdb" > nul 2> nul
 
 echo Cleaning dbg files done
