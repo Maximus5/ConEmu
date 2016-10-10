6cde7fc20aa07d3f4cb8d172bbc3b8541ae07019
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-10-10 21:44:27 +0300

    161009a release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,9 +1,9 @@
-// 161009
+// 161009a
 #define MVV_1 16
 #define MVV_2 10
 #define MVV_3 9
-#define MVV_4 0
-#define MVV_4a ""
+#define MVV_4 1
+#define MVV_4a "a"
 #undef MVV_git
 
 #include "version_stage.h"
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 16,10,9,0
-#define CONEMUVERS "161009"
-#define CONEMUVERL L"161009"
-#define MSI86 "../ConEmu.161009.x86.msi"
-#define MSI64 "../ConEmu.161009.x64.msi"
+#define CONEMUVERN 16,10,9,1
+#define CONEMUVERS "161009a"
+#define CONEMUVERL L"161009a"
+#define MSI86 "../ConEmu.161009a.x86.msi"
+#define MSI64 "../ConEmu.161009a.x64.msi"
