e088ac8bc6ff8e03f4f42d62f60020988fd38a1c
Maximus5 <ConEmu.Maximus5@gmail.com>
2016-10-17 00:37:19 +0300

    Far Macro. Some improvements in ConEmu.CtrlNumber.lua

--- Release/ConEmu/Far3_lua/ConEmu.CtrlNumber.lua
+++ Release/ConEmu/Far3_lua/ConEmu.CtrlNumber.lua
@@ -2,17 +2,52 @@
 -- Place this file into your %FARPROFILE%\Macros\scripts
 
 -- When tab switching in ConEmu is configured to Ctrl+Number
--- this macro will help to switch Far Manager panel modes
--- LCtrl-Shift-1 to ‘Brief’, LCtrl-Shift-2 to ‘Medium’, etc.
+-- this macro will help to use Ctrl+Number combinations
 
 Macro
 {
-  area="Common";
+  area="Shell";
   key="/LCtrlShift[0-9]/";
-  description="Far: Map all LCtrl+Shift+Digit to LCtrl+Digit";
+  description="Panels: Switch panel modes with LCtrl+Shift+Number";
 action = function()
   -- BUG? Following is expected to be working, but not... Far Panels ignores it
   -- Keys("LCtrl"..akey(1):sub(-1))
+  Keys("Ctrl"..akey(1):sub(-1))
+end
+}
+
+Macro
+{
+  area="Shell";
+  key="/RCtrlShift[0-9]/";
+  description="Panels: No action";
+action = function()
+  return
+end
+}
+
+Macro
+{
+  area="Editor Viewer";
+  key="/RCtrlShift[0-9]/";
+  description="Editor: Set std.bookmark with RCtrl+Shift+Number +++";
+action = function()
+  -- Extend std.bookmarks with stack bookmarks
+  BM.Add()
+  -- And set new std.bookmark
+  Keys("CtrlShift"..akey(1):sub(-1))
+end
+}
+
+Macro
+{
+  area="Editor Viewer";
+  key="/RCtrl[0-9]/";
+  description="Editor: Jump to std.bookmark with RCtrl+Number";
+action = function()
+  -- Store previous position in stack bookmarks
+  BM.Add()
+  -- And jump to std.bookmark
   Keys("Ctrl"..akey(1):sub(-1))
 end
 }
