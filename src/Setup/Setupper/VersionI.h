1b6bb22e8be3b0d29b10f23abfdd31aed44a756f
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-01-19 11:48:58 +0300

    170118 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 16,12,6,0
-#define CONEMUVERS "161206"
-#define CONEMUVERL L"161206"
-#define MSI86 "../ConEmu.161206.x86.msi"
-#define MSI64 "../ConEmu.161206.x64.msi"
+#define CONEMUVERN 17,1,18,0
+#define CONEMUVERS "170118"
+#define CONEMUVERL L"170118"
+#define MSI86 "../ConEmu.170118.x86.msi"
+#define MSI64 "../ConEmu.170118.x64.msi"
