5478c10a02f4516dd89332467ba200fc2934059d
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-03-27 12:43:21 +0300

    170326 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 17,3,16,0
-#define CONEMUVERS "170316"
-#define CONEMUVERL L"170316"
-#define MSI86 "../ConEmu.170316.x86.msi"
-#define MSI64 "../ConEmu.170316.x64.msi"
+#define CONEMUVERN 17,3,26,0
+#define CONEMUVERS "170326"
+#define CONEMUVERL L"170326"
+#define MSI86 "../ConEmu.170326.x86.msi"
+#define MSI64 "../ConEmu.170326.x64.msi"
