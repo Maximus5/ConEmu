d9739b8e6857df27e86479515ef79edc7f51ed81
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-05-13 20:10:43 +0300

    Internal. Build script

--- src/release_.bat
+++ src/release_.bat
@@ -1,31 +1,58 @@
 @echo off
 
+setlocal
+
 cd /d "%~dp0"
 
-if "%~1" == "" (
-  cmd /c ""%~0" build_x86 -new_console"
-  rem TODO: somehow wait for created consoles
-  exit /b 1
-)
+set x32_build_flag="%~dp0.x32_build_flag"
+set x64_build_flag="%~dp0.x64_build_flag"
 
+set target=clean,build
+rem set target=build
 
 if errorlevel 1 goto err
 
-if "%~1" == "build_x86" (
-  cmd /c ""%~0" build_x64 -new_console:sVb"
-  rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
-  msbuild /m:3 /p:Configuration=Release,Platform=Win32 CE14.sln
-  goto err
-)
+if "%~1" == "" goto do_both
+if "%~1" == "build_x86" goto build_x86
+if "%~1" == "build_x64" goto build_x64
+echo Unsupported switch: "%~1"
+goto err
 
-if "%~1" == "build_x64" (
-  rem call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-  call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
-  msbuild /m:3 /p:Configuration=Release,Platform=x64 CE14.sln
-  goto err
-)
 
+
+:do_both
+echo Waiting for x32 build > %x32_build_flag%
+echo Waiting for x64 build > %x64_build_flag%
+cmd /c ""%~0" build_x86 -new_console:s30Vb"
+cmd /c ""%~0" build_x64 -new_console:s50Vb"
+Echo Waiting for build process
+:do_wait
+timeout /t 5 > nul
+if exist %x32_build_flag% goto do_wait
+if exist %x64_build_flag% goto do_wait
+:all_done
+exit /b 1
+
+
+
+
+
+:build_x86
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars32.bat"
+msbuild CE17.sln /m:4 /p:Configuration=Release,Platform=Win32 /t:%target%
+if errorlevel 1 goto err
+del %x32_build_flag%
 goto :EOF
+
+
+:build_x64
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\VC\Auxiliary\Build\vcvars64.bat"
+msbuild CE17.sln /m:4 /p:Configuration=Release,Platform=x64 /t:%target%
+if errorlevel 1 goto err
+del %x64_build_flag%
+goto :EOF
+
+
 :err
+endlocal
 pause
