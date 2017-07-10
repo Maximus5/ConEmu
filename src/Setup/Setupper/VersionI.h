81ed19b7e64b9b9be4452ea96a38e8be2a1b9a93
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-07-10 13:37:51 +0300

    170709 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 17,7,5,0
-#define CONEMUVERS "170705"
-#define CONEMUVERL L"170705"
-#define MSI86 "../ConEmu.170705.x86.msi"
-#define MSI64 "../ConEmu.170705.x64.msi"
+#define CONEMUVERN 17,7,9,0
+#define CONEMUVERS "170709"
+#define CONEMUVERL L"170709"
+#define MSI86 "../ConEmu.170709.x86.msi"
+#define MSI64 "../ConEmu.170709.x64.msi"
