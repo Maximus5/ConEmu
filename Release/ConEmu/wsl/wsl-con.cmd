a2fad4a219aca79ffd772afa8dfd9b523e70e7ca
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-07-30 21:46:51 +0300

    Internal. Update batch to run wsl sample using preinstalled files

--- Release/ConEmu/wsl/wsl-con.cmd
+++ Release/ConEmu/wsl/wsl-con.cmd
@@ -22,73 +22,26 @@ setlocal
 
 if exist "%~dp0wslbridge-backend" goto wsl_ready
 
-call cecho /yellow "wslbridge is not installed!"
-echo https://cygwin.com/licensing.html
-echo https://github.com/rprichard/wslbridge/blob/master/LICENSE.txt
-echo https://github.com/Maximus5/cygwin-connector/blob/master/License.txt
-set /P confirm="Type YES to accept licenses: "
-if /I "%confirm%" == "YES" goto do_install
+call cecho /yellow "wslbridge is not installed! download latest ConEmu distro"
 goto err
-
-:do_install
-set "wsltty_url=https://github.com/mintty/wsltty/releases/download/0.6.0/wsltty-0.6.0-install.exe"
-set "connector_url=https://github.com/Maximus5/cygwin-connector/releases/download/v0.7.4/terminals.v0.7.4.7z"
-set "szip_url=https://github.com/ConEmu/ConEmu.github.io/releases/download/7zip-9.20/7za.exe"
-set "boot_url=https://ConEmu.github.io/wsl-boot.sh"
-set "clr_url=https://ConEmu.github.io/256colors2.pl"
-
-set "dl_tmp=%~dp0tmp"
-if not exist "%dl_tmp%" md "%dl_tmp%"
-cd /d "%~dp0"
-
-call :do_clean > nul
-
-call cecho /green "Downloading wsltty"
-ConEmuC -download "%wsltty_url%" "%dl_tmp%\wsltty.7z" 2>nul
-if errorlevel 1 goto err
-call cecho /green "Downloading cygwin-connector"
-ConEmuC -download "%connector_url%" "%dl_tmp%\terminals.7z" 2>nul
-if errorlevel 1 goto err
-call cecho /green "Downloading 7zip tool"
-ConEmuC -download "%szip_url%" "%dl_tmp%\7za.exe" 2>nul
-if errorlevel 1 goto err
-call cecho /green "Downloading bash startup script"
-ConEmuC -download "%boot_url%" wsl-boot.sh 2>nul
-if errorlevel 1 goto err
-call cecho /green "Downloading 256colors sample"
-ConEmuC -download "%clr_url%" 256colors2.pl 2>nul
-if errorlevel 1 goto err
-
-call cecho /green "Extracting files"
-"%dl_tmp%\7za.exe" e "%dl_tmp%\terminals.7z" conemu-cyg-32.exe 1>nul
-if errorlevel 1 goto err
-"%dl_tmp%\7za.exe" e "%dl_tmp%\wsltty.7z" wslbridge.exe cygwin1.dll wslbridge-backend 1>nul
-if errorlevel 1 goto err
-
-call :do_clean > nul
-rd "%dl_tmp%"
-
-if exist "%~dp0wslbridge-backend" goto wsl_ready
-goto err
-
-:do_clean
-if exist "%dl_tmp%\wsltty.7z" del "%dl_tmp%\wsltty.7z"
-if exist "%dl_tmp%\terminals.7z" del "%dl_tmp%\terminals.7z"
-if exist "%dl_tmp%\7za.exe" del "%dl_tmp%\7za.exe"
-goto :EOF
 
 :wsl_ready
+echo 1: "%~1"
 if "%~1" == "-run" goto do_run
-ConEmuC -c "-new_console:d:%~dp0" "%~0" -new_console:n:h9999:C:"%LOCALAPPDATA%\lxss\bash.ico" -run
+ConEmuC -c "-new_console:d:%~dp0" "%~0" -new_console:c:h9999:C:"%LOCALAPPDATA%\lxss\bash.ico" -run
 goto :EOF
 
 :do_run
+cd /d "%~dp0"
+set "PATH=%~dp0;%PATH%"
 call SetEscChar
 echo %ESC%[9999H
-conemu-cyg-32.exe ./wslbridge.exe -t ./wsl-boot.sh
+"%~dp0..\conemu-cyg-64.exe" --wsl -t ./wsl-boot.sh
 goto :EOF
 
 :err
 call cecho "wslbridge-backend was not installed properly"
 timeout 10
+exit /b 1
 goto :EOF
+
