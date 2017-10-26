c40988909d342dc950bab3df6e45d09fb8b3d2ee
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-10-26 13:51:27 +0300

    171025 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 171024
+// 171025
 #define MVV_1 17
 #define MVV_2 10
-#define MVV_3 24
+#define MVV_3 25
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 17,9,10,0
-#define CONEMUVERS "170910"
-#define CONEMUVERL L"170910"
-#define MSI86 "../ConEmu.170910.x86.msi"
-#define MSI64 "../ConEmu.170910.x64.msi"
+#define CONEMUVERN 17,10,25,0
+#define CONEMUVERS "171025"
+#define CONEMUVERL L"171025"
+#define MSI86 "../ConEmu.171025.x86.msi"
+#define MSI64 "../ConEmu.171025.x64.msi"
