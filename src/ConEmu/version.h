dcf4d599c00c19dd9f346a64235787f2db9ea388
Maximus5 <ConEmu.Maximus5@gmail.com>
2017-12-04 03:07:41 +0300

    171203 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 171117
+// 171203
 #define MVV_1 17
-#define MVV_2 11
-#define MVV_3 17
+#define MVV_2 12
+#define MVV_3 3
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 17,11,17,0
-#define CONEMUVERS "171117"
-#define CONEMUVERL L"171117"
-#define MSI86 "../ConEmu.171117.x86.msi"
-#define MSI64 "../ConEmu.171117.x64.msi"
+#define CONEMUVERN 17,12,3,0
+#define CONEMUVERS "171203"
+#define CONEMUVERL L"171203"
+#define MSI86 "../ConEmu.171203.x86.msi"
+#define MSI64 "../ConEmu.171203.x64.msi"
