f3e6679efe6c840da7f3b13036ae4aae9da9b878
Maximus5 <ConEmu.Maximus5@gmail.com>
2018-01-15 12:44:07 +0300

    180114 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 180111
+// 180114
 #define MVV_1 18
 #define MVV_2 1
-#define MVV_3 11
+#define MVV_3 14
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 18,1,11,0
-#define CONEMUVERS "180111"
-#define CONEMUVERL L"180111"
-#define MSI86 "../ConEmu.180111.x86.msi"
-#define MSI64 "../ConEmu.180111.x64.msi"
+#define CONEMUVERN 18,1,14,0
+#define CONEMUVERS "180114"
+#define CONEMUVERL L"180114"
+#define MSI86 "../ConEmu.180114.x86.msi"
+#define MSI64 "../ConEmu.180114.x64.msi"
