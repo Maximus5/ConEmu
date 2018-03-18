9d86d89b28195ba276851cb31170fce5398bc08e
Maximus5 <ConEmu.Maximus5@gmail.com>
2018-03-18 18:35:17 +0300

    Internal. Implement RAII Ole/Com initializator

---
+++ src/ConEmu/OleInitializer.cpp
@@ -0,0 +1,64 @@
+
+/*
+Copyright (c) 2017-Present Maximus5
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#define HIDE_USE_EXCEPTION_INFO
+#define SHOWDEBUGSTR
+
+#include "Header.h"
+#include <Ole2.h>
+#include "OleInitializer.h"
+
+OleInitializer::OleInitializer()
+{
+}
+
+OleInitializer::OleInitializer(bool init)
+{
+	if (init)
+		Initialize();
+}
+
+OleInitializer::~OleInitializer()
+{
+	if (isInitialized())
+		OleUninitialize();
+}
+
+bool OleInitializer::Initialize()
+{
+	if (isInitialized())
+		return true;
+
+	initialized = OleInitialize(NULL);
+	return isInitialized();
+}
+
+bool OleInitializer::isInitialized() const
+{
+	return (initialized == S_OK) || (initialized == S_FALSE);
+}
