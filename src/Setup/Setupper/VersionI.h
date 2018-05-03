1f076764415ae88694edad5f130a2d2d8dc52936
Maximus5 <ConEmu.Maximus5@gmail.com>
2018-05-04 02:46:30 +0300

    180503 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 18,4,29,0
-#define CONEMUVERS "180429"
-#define CONEMUVERL L"180429"
-#define MSI86 "../ConEmu.180429.x86.msi"
-#define MSI64 "../ConEmu.180429.x64.msi"
+#define CONEMUVERN 18,5,3,0
+#define CONEMUVERS "180503"
+#define CONEMUVERL L"180503"
+#define MSI86 "../ConEmu.180503.x86.msi"
+#define MSI64 "../ConEmu.180503.x64.msi"
