52f498eef2c8d44bac6b37326f9ae8f79b8bb8df
Maximus5 <ConEmu.Maximus5@gmail.com>
2019-03-06 01:24:59 +0300

    Update ChocolateyAbout.cmd to latest installation cmd.

--- Release/ConEmu/Addons/ChocolateyAbout.cmd
+++ Release/ConEmu/Addons/ChocolateyAbout.cmd
@@ -1,21 +1,23 @@
 @echo off
 
-rem ****************************************************************
-rem ** If you don't know about Chocolatey (apt-get style manager) **
-rem ** read about it here: https://chocolatey.org/                **
-rem ** If you are sure about installing it, execute the following **
-rem ** one-line command:                                          **
-rem ** powershell -NoProfile -ExecutionPolicy unrestricted        **
-rem ** -Command "iex ((new-object net.webclient).DownloadString   **
-rem ** ('https://chocolatey.org/install.ps1'))"                   **
-rem ** && SET PATH=%PATH%;%systemdrive%\chocolatey\bin            **
-rem ****************************************************************
+rem ************************************************************
+rem If you don't know about Chocolatey (apt-get style manager)
+rem read about it here: https://chocolatey.org/
+rem If you are sure about installing it, execute the following
+rem one-line command:
+rem one-line command (select it with mouse and press RClick):
+rem powershell.exe -NoProfile -InputFormat None -ExecutionPolicy
+rem   Bypass -Command "iex ((New-Object System.Net.WebClient).
+rem   DownloadString('https://chocolatey.org/install.ps1'))"
+rem   ^&^& SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+rem ************************************************************
 
 setlocal
 if "%ConEmuANSI%" == "ON" (
   set green=[1;32;40m
   set red=[1;31;40m
   set gray=[0m
+  set yellow=[1;33;40m
   set white=[1;37;40m
 ) else (
   set green=
@@ -24,22 +26,22 @@ if "%ConEmuANSI%" == "ON" (
   set white=
 )
 rem Do not print ending '**' because it's not so easy to pad it with spaces
-echo %green%**************************************************************
-echo %green%** %gray%If you%red% don't know about Chocolatey %gray%(apt-get style manager) %green%
-echo %green%** %red%read about it%gray% here:%green% https://chocolatey.org/                %green%
+echo %green%************************************************************%gray%
+echo If you don't know about %yellow%Chocolatey %gray%(apt-get style manager)
+echo %yellow%read about it%gray% here:%green% https://chocolatey.org/
 if DEFINED ChocolateyInstall (
   if EXIST "%ChocolateyInstall%\choco.exe" (
-    echo %green%** %gray%Chocolatey is already installed on your PC:                %green%
-    echo %green%** %red%%ChocolateyInstall% %green%
+    echo %yellow%Chocolatey is already installed on your PC:%gray%
+    echo %green%  %ChocolateyInstall% %gray%
     goto done
   )
 )
-echo %green%** %gray%If you are sure about installing it, execute the following %green%
-echo %green%** %red%one-line command:                                          %green%
-echo %green%** %white%powershell -NoProfile -ExecutionPolicy unrestricted        %green%
-echo %green%**  %white%-Command "iex ((new-object net.webclient).                %green%
-echo %green%**  %white%DownloadString('https://chocolatey.org/install.ps1'))"    %green%
-echo %green%**  %white%^&^& SET PATH=%%PATH%%;%%ALLUSERSPROFILE%%\chocolatey\bin       %green%
+echo %gray%If you are sure about installing it, execute the following
+echo %red%one-line command%gray% (%yellow%select it with mouse and press RClick%gray%):
+echo %white%powershell.exe -NoProfile -InputFormat None -ExecutionPolicy
+echo %white%  Bypass -Command "iex ((New-Object System.Net.WebClient).
+echo %white%  DownloadString('https://chocolatey.org/install.ps1'))"
+echo %white%  ^&^& SET "PATH=%%PATH%%;%%ALLUSERSPROFILE%%\chocolatey\bin"
 :done
-echo %green%**************************************************************%gray%
+echo %green%************************************************************%gray%
 endlocal
