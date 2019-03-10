a75bdde2b4c7a18fc46875c1e3c5206eac2e67b1
Maximus5 <ConEmu.Maximus5@gmail.com>
2019-03-11 01:47:49 +0300

    190310 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 19,3,3,0
-#define CONEMUVERS "190303"
-#define CONEMUVERL L"190303"
-#define MSI86 "../ConEmu.190303.x86.msi"
-#define MSI64 "../ConEmu.190303.x64.msi"
+#define CONEMUVERN 19,3,10,0
+#define CONEMUVERS "190310"
+#define CONEMUVERL L"190310"
+#define MSI86 "../ConEmu.190310.x86.msi"
+#define MSI64 "../ConEmu.190310.x64.msi"
