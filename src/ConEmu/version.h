be9695f5b565c28379237a5fdea3abab7fea3e3a
Maximus5 <ConEmu.Maximus5@gmail.com>
2019-05-27 01:59:42 +0300

    190526 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 190331
+// 190526
 #define MVV_1 19
-#define MVV_2 3
-#define MVV_3 31
+#define MVV_2 5
+#define MVV_3 26
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 19,3,31,0
-#define CONEMUVERS "190331"
-#define CONEMUVERL L"190331"
-#define MSI86 "../ConEmu.190331.x86.msi"
-#define MSI64 "../ConEmu.190331.x64.msi"
+#define CONEMUVERN 19,5,26,0
+#define CONEMUVERS "190526"
+#define CONEMUVERL L"190526"
+#define MSI86 "../ConEmu.190526.x86.msi"
+#define MSI64 "../ConEmu.190526.x64.msi"
