83fce2ce2b807f0eb055c30f908769be6ed24c3c
Maximus5 <ConEmu.Maximus5@gmail.com>
2020-11-01 19:03:52 +0100

    201101 release files

--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 20,10,11,0
-#define CONEMUVERS "201011"
-#define CONEMUVERL L"201011"
-#define MSI86 "../ConEmu.201011.x86.msi"
-#define MSI64 "../ConEmu.201011.x64.msi"
+#define CONEMUVERN 20,11,1,0
+#define CONEMUVERS "201101"
+#define CONEMUVERL L"201101"
+#define MSI86 "../ConEmu.201101.x86.msi"
+#define MSI64 "../ConEmu.201101.x64.msi"
