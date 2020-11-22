c2085dce5f9e18605ff089d1194bac3bf8fd2f12
Maximus5 <ConEmu.Maximus5@gmail.com>
2020-11-23 00:29:37 +0100

    Internal. Fix unit tests

--- src/UnitTests/test1.cpp
+++ src/UnitTests/test1.cpp
@@ -4,6 +4,18 @@
 bool gbVerifyFailed = false;
 bool gbVerifyStepFailed = false;
 bool gbVerifyVerbose = false;
+bool gbVerifyIgnoreAsserts = false;
+
+bool FileExistsMock(const wchar_t* asFilePath, uint64_t * pnSize, bool& result)
+{
+	return false;
+}
+
+bool SearchPathMock(LPCWSTR path, LPCWSTR fileName, LPCWSTR extension, CEStr& resultPath, int& rc)
+{
+	return false;
+}
+
 
 int main(int argc, char** argv)
 {
