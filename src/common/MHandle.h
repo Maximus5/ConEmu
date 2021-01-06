7920e7127e0e6701ab311969b7a4470971749c49
Maximus5 <ConEmu.Maximus5@gmail.com>
2021-01-06 18:03:18 +0100

    Internal. HANDLE and HWND wrappers

---
+++ src/common/MHandle.h
@@ -0,0 +1,111 @@
+﻿
+/*
+Copyright (c) 2009-present Maximus5
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#pragma once
+
+#include "defines.h"
+#include <functional>
+
+class MHandle final
+{
+private:
+	HANDLE handle_ = nullptr;
+	std::function<void(HANDLE)> closeFunc_ = nullptr;
+
+public:
+	// ReSharper disable once CppNonExplicitConvertingConstructor
+	// ReSharper disable once CppParameterMayBeConst
+	MHandle(HANDLE handle)
+		: handle_(handle)
+	{
+	}
+
+	MHandle(HANDLE handle, std::function<void(HANDLE)> closeFunc)
+		: handle_(handle), closeFunc_(std::move(closeFunc))
+	{
+	}
+
+	~MHandle()
+	{
+		Close();
+	}
+
+	// non-copyable
+	MHandle(const MHandle&) = delete;
+	MHandle& operator=(const MHandle&) = delete;
+	// movable
+	MHandle(MHandle&&) = default;
+	MHandle& operator=(MHandle&&) = default;
+
+public:
+	bool HasHandle() const
+	{
+		return handle_ != nullptr && handle_ != INVALID_HANDLE_VALUE;
+	}
+
+	operator HANDLE() const
+	{
+		return handle_;
+	}
+
+	HANDLE GetHandle() const
+	{
+		return handle_;
+	}
+
+	HANDLE SetHandle(HANDLE handle)
+	{
+		if (handle_ != handle)
+			Close();
+		handle_ = handle;
+		closeFunc_ = nullptr;
+		return handle_;
+	}
+
+	HANDLE SetHandle(HANDLE handle, std::function<void(HANDLE)> closeFunc)
+	{
+		if (handle_ != handle)
+			Close();
+		handle_ = handle;
+		closeFunc_ = std::move(closeFunc);
+		return handle_;
+	}
+
+	void Close()
+	{
+		if (HasHandle() && closeFunc_ != nullptr)
+		{
+			HANDLE handle = nullptr;
+			std::swap(handle, handle_);
+			closeFunc_(handle);
+		}
+		closeFunc_ = nullptr;
+		handle_ = nullptr;
+	}
+};
---
+++ src/common/MWnd.h
@@ -0,0 +1,95 @@
+﻿
+/*
+Copyright (c) 2009-present Maximus5
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in the
+   documentation and/or other materials provided with the distribution.
+3. The name of the authors may not be used to endorse or promote products
+   derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR ''AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#pragma once
+
+#include "defines.h"
+
+class MWnd final
+{
+private:
+	HWND hwnd_ = nullptr;
+public:
+	// ReSharper disable once CppNonExplicitConvertingConstructor
+	// ReSharper disable once CppParameterMayBeConst
+	MWnd(HWND hwnd)
+		: hwnd_(hwnd)
+	{
+	}
+
+	~MWnd() = default;
+
+	// copyable
+	MWnd(const MWnd&) = default;
+	MWnd& operator=(const MWnd&) = default;
+	// movable
+	MWnd(MWnd&&) = default;
+	MWnd& operator=(MWnd&&) = default;
+
+public:
+	bool HasHandle() const
+	{
+		return (hwnd_ != nullptr);
+	}
+
+	operator HWND() const
+	{
+		return hwnd_;
+	}
+
+	HWND GetHwnd() const
+	{
+		return hwnd_;
+	}
+
+	// All windows handles in Windows (64-bit or 32-bit) are available to both 32-bit and 64-bit processes
+	DWORD GetDword() const
+	{
+		const auto dword = LODWORD(hwnd_);
+		if (reinterpret_cast<DWORD_PTR>(hwnd_) != dword)
+		{
+			_ASSERTE(reinterpret_cast<DWORD_PTR>(hwnd_) == dword);
+			return 0;
+		}
+		return dword;
+	}
+
+	// ReSharper disable once CppParameterMayBeConst
+	HWND SetHwnd(HWND hwnd)
+	{
+		hwnd_ = hwnd;
+		return hwnd_;
+	}
+
+	HWND SetHwnd(DWORD portableHandle)
+	{
+		hwnd_ = reinterpret_cast<HWND>(static_cast<DWORD_PTR>(portableHandle));
+		return hwnd_;
+	}
+};
