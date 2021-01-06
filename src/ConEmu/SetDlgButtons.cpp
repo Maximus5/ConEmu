8e6b1cc85840118896d6a996d99054221cad1f30
Oliver Kopp <kopp.dev@gmail.com>
2021-01-06 17:47:18 +0100

    Change clink URL to maintained fork

--- src/ConEmu/SetDlgButtons.cpp
+++ src/ConEmu/SetDlgButtons.cpp
@@ -3140,7 +3140,7 @@ void CSetDlgButtons::OnBtn_UseClink(HWND hDlg, WORD CB, BYTE uCheck)
 		checkDlgButton(hDlg, cbUseClink, BST_UNCHECKED);
 		wchar_t szErrInfo[MAX_PATH+200];
 		swprintf_c(szErrInfo,
-			L"Clink was not found in '%s\\clink'. Download and unpack clink files\nhttps://mridgers.github.io/clink/\n\n"
+			L"Clink was not found in '%s\\clink'. Download and unpack clink files\nhttps://chrisant996.github.io/clink/\n\n"
 			L"Note that you don't need to check 'Use clink'\nif you already have set up clink globally.",
 			gpConEmu->ms_ConEmuBaseDir);
 		MsgBox(szErrInfo, MB_ICONSTOP|MB_SYSTEMMODAL, nullptr, ghOpWnd);
@@ -3155,7 +3155,7 @@ void CSetDlgButtons::OnBtn_ClinkWebPage(HWND hDlg, WORD CB, BYTE uCheck)
 {
 	_ASSERTE(CB==cbClinkWebPage);
 
-	ShellExecute(nullptr, L"open", L"https://mridgers.github.io/clink/", nullptr, nullptr, SW_SHOWNORMAL);
+	ShellExecute(nullptr, L"open", L"https://chrisant996.github.io/clink/", nullptr, nullptr, SW_SHOWNORMAL);
 
 } // cbClinkWebPage
 
