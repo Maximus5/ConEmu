ef684f3dca40a47e0e1258190853186dabc1cda8
Maximus5 <ConEmu.Maximus5@gmail.com>
2021-01-09 22:57:44 +0100

    l10n: Italian translation was started.

---
+++ Deploy/l10n_add_lang.cmd
@@ -0,0 +1,38 @@
+@echo off
+
+if "%~2" == "" (
+  echo [1;31;40mLanguageID and LanguageName should be specified[0m
+  goto :err
+)
+
+setlocal
+
+if exist "%~dp0user_env.cmd" (
+  call "%~dp0user_env.cmd"
+) else (
+  call "%~dp0user_env.default.cmd"
+)
+
+call "%UPLOADERS%init_env.cmd"
+
+pushd "%~dp0.."
+
+set L10N_PATH=Release/ConEmu/ConEmu.l10n
+set PSEUDO_PATH=src\l10n\ConEmu.l10n
+
+call "%PYTHON3%" "%~dp0l10n.py" --l10n %L10N_PATH% --add-lng %1 %2 --write-l10n
+if errorlevel 1 (
+  echo [1;31;40mFailed to write l10n file[0m
+  goto :err
+)
+
+goto fin
+
+:err
+endlocal
+popd
+exit /b 1
+
+:fin
+endlocal
+popd
