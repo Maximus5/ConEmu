Maximus5/ConEmu

commit 82a2ca16b01a6b852898a21386ab6df6f4e949c7

Fix running from cmd prompt: `ConEmuC -c {bash}`.
  Note: ConEmuC is 32-bit app, it failed to check if wsl.exe exists in 64-bit system32 folder.

src/ConEmuCD/WorkerBase.cpp
@@ -317,7 +317,34 @@ int WorkerBase::PostProcessPrepareCommandLine()
 
 	LPCWSTR lsCmdLine = gpConsoleArgs->command_.c_str(L"");
 
+	bool needSetEnv = false;
 	if (gState.runMode_ == RunMode::Server)
+	{
+		needSetEnv = true;
+	}
+
+	if (lsCmdLine && (lsCmdLine[0] == TaskBracketLeft) && wcschr(lsCmdLine, TaskBracketRight))
+	{
+		LogFunction(L"ExpandTaskCmd {bash} -> wsl.exe");
+		// Allow smth like: ConEmuC -c {Far} /e text.txt
+		// _ASSERTE(FALSE && "Continue to ExpandTaskCmd");
+		gpConsoleArgs->taskCommand_ = ExpandTaskCmd(lsCmdLine);
+		if (!gpConsoleArgs->taskCommand_.IsEmpty())
+		{
+			lsCmdLine = gpConsoleArgs->taskCommand_.c_str();
+			needSetEnv = true;
+		}
+	}
+
+	// Cut and process the "-new_console" / "-cur_console"
+	const CEStr stripNewConsole(lsCmdLine);
+	SafeFree(args_.pszSpecialCmd);
+	args_.pszSpecialCmd = stripNewConsole.data();
+	args_.ProcessNewConArg();
+	args_.pszSpecialCmd = nullptr; // this point to global gpszRunCmd
+	lsCmdLine = stripNewConsole.c_str();
+
+	if (needSetEnv)
 	{
 		LogFunction(L"ProcessSetEnvCmd {set, title, chcp, etc.}");
 		// Console may be started as follows:
@@ -330,23 +357,6 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		auto* pSetEnv = EnvCmdProcessor();
 		CStartEnvTitle setTitleVar(&gpszForcedTitle);
 		ProcessSetEnvCmd(lsCmdLine, pSetEnv, &setTitleVar);
-	}
-
-	if (lsCmdLine && (lsCmdLine[0] == TaskBracketLeft) && wcschr(lsCmdLine, TaskBracketRight))
-	{
-		LogFunction(L"ExpandTaskCmd {bash} -> wsl.exe");
-		// Allow smth like: ConEmuC -c {Far} /e text.txt
-		// _ASSERTE(FALSE && "Continue to ExpandTaskCmd");
-		gpConsoleArgs->taskCommand_ = ExpandTaskCmd(lsCmdLine);
-		if (!gpConsoleArgs->taskCommand_.IsEmpty())
-		{
-			lsCmdLine = gpConsoleArgs->taskCommand_.c_str();
-
-			LogFunction(L"ProcessSetEnvCmd {by task}");
-			auto* pSetEnv = EnvCmdProcessor();
-			CStartEnvTitle setTitleVar(&gpszForcedTitle);
-			ProcessSetEnvCmd(lsCmdLine, pSetEnv, &setTitleVar);
-		}
 	}
 
 	if (gState.runMode_ == RunMode::Comspec)
@@ -415,7 +425,7 @@ int WorkerBase::PostProcessPrepareCommandLine()
 	CheckNeedSkipWowChange(lsCmdLine);
 	#endif
 
-	int nCmdLine = lstrlenW(lsCmdLine);
+	int nCmdLine = lsCmdLine ? lstrlenW(lsCmdLine) : 0;
 
 	if (!gState.runViaCmdExe_)
 	{
@@ -430,7 +440,7 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		LPCWSTR pszFind = GetComspecFromEnvVar(gszComSpec, countof(gszComSpec));
 		if (!pszFind || !wcschr(pszFind, L'\\') || !FileExists(pszFind))
 		{
-			_ASSERTE("cmd.exe not found!");
+			_ASSERTE(FALSE && "cmd.exe not found!");
 			_printf("Can't find cmd.exe!\n");
 			return CERR_CMDEXENOTFOUND;
 		}
@@ -492,17 +502,12 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		if (pszEndQ && *pszEndQ == L'"') *pszEndQ = 0;
 	}
 
-	// Cut and process the "-new_console" / "-cur_console"
-	SafeFree(args_.pszSpecialCmd);
-	args_.pszSpecialCmd = gpszRunCmd;
-	args_.ProcessNewConArg();
-	args_.pszSpecialCmd = nullptr; // this point to global gpszRunCmd
-	// Если указана рабочая папка
+	// If working folder was specified
 	if (args_.pszStartupDir && *args_.pszStartupDir)
 	{
 		SetCurrentDirectory(args_.pszStartupDir);
 	}
-	//
+
 	gbRunInBackgroundTab = (args_.BackgroundTab == crb_On);
 	if (args_.BufHeight == crb_On)
 	{
@@ -510,7 +515,7 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		gnBufferHeight = args_.nBufHeight;
 		gbParmBufSize = TRUE;
 	}
-	// DosBox?
+
 	if (args_.ForceDosBox == crb_On)
 	{
 		gState.dosbox_.use_ = TRUE;
@@ -1715,7 +1720,7 @@ void WorkerBase::CheckNeedSkipWowChange(LPCWSTR asCmdLine)
 
 					if (lstrcmpiW(szApp, szSysNative) == 0)
 					{
-						gState.bSkipWowChange_ = TRUE;
+						gState.bSkipWowChange_ = true;
 					}
 				}
 			}

src/ConEmuCD/Actions.cpp
@@ -33,6 +33,7 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #ifdef _DEBUG
 //#define SHOW_ATTACH_MSGBOX
 //#define SHOW_OUTPUT_MSGBOX
+//#define SHOW_INJECT_MSGBOX
 #endif
 
 #include "../common/Common.h"
@@ -226,12 +227,17 @@ int DoInjectHooks(const CEStr& asCmdArg)
 
 
 	#ifdef SHOW_INJECT_MSGBOX
-	wchar_t szDbgMsg[512], szTitle[128];
-	PROCESSENTRY32 pinf;
-	GetProcessInfo(pi.dwProcessId, &pinf);
-	swprintf_c(szTitle, L"ConEmuCD PID=%u", GetCurrentProcessId());
-	swprintf_c(szDbgMsg, L"InjectsTo PID=%s {%s}\nConEmuCD PID=%u", asCmdArg ? asCmdArg : L"", pinf.szExeFile, GetCurrentProcessId());
-	MessageBoxW(nullptr, szDbgMsg, szTitle, MB_SYSTEMMODAL);
+	{
+		wchar_t szDbgMsg[512] = L"", szTitle[128] = L"";
+		PROCESSENTRY32W procInfo{};
+		GetProcessInfo(pi.dwProcessId, procInfo);
+		swprintf_c(szTitle, L"ConEmuCD PID=%u", GetCurrentProcessId());
+		swprintf_c(szDbgMsg, L"InjectsTo PID=%s {%s}\nConEmuCD PID=%u", asCmdArg.c_str(L""), procInfo.szExeFile, GetCurrentProcessId());
+		if (MessageBoxW(nullptr, szDbgMsg, szTitle, MB_SYSTEMMODAL | MB_OKCANCEL) != IDOK)
+		{
+			return CERR_HOOKS_FAILED;
+		}
+	}
 	#endif
 
 
@@ -244,8 +250,8 @@ int DoInjectHooks(const CEStr& asCmdArg)
 			return CERR_HOOKS_WAS_SET;
 		}
 
-		// Ошибку (пока во всяком случае) лучше показать, для отлова возможных проблем
-		DWORD nErrCode = GetLastError();
+		// It's better to show an error message
+		const DWORD nErrCode = GetLastError();
 		//_ASSERTE(iHookRc == 0); -- ассерт не нужен, есть MsgBox
 		wchar_t szDbgMsg[255], szTitle[128];
 		swprintf_c(szTitle, L"ConEmuC[%u], PID=%u", WIN3264TEST(32,64), GetCurrentProcessId());
@@ -255,11 +261,11 @@ int DoInjectHooks(const CEStr& asCmdArg)
 	else
 	{
 		//_ASSERTE(pi.hProcess && pi.hThread && pi.dwProcessId && pi.dwThreadId);
-		wchar_t szDbgMsg[512], szTitle[128];
+		wchar_t szDbgMsg[512] = L"", szTitle[128] = L"";
 		swprintf_c(szTitle, L"ConEmuC, PID=%u", GetCurrentProcessId());
 		swprintf_c(szDbgMsg, L"ConEmuC.X, PID=%u\nCmdLine parsing FAILED (%u,%u,%u,%u,%u)!\n%s",
 			GetCurrentProcessId(), LODWORD(pi.hProcess), LODWORD(pi.hThread), pi.dwProcessId, pi.dwThreadId, lbForceGui, //-V205
-			asCmdArg);
+			asCmdArg.c_str(L""));
 		MessageBoxW(nullptr, szDbgMsg, szTitle, MB_SYSTEMMODAL);
 	}
 

src/ConEmuCD/ConsoleMain.cpp
@@ -242,8 +242,8 @@ int ShowInjectRemoteMsg(int nRemotePID, LPCWSTR asCmdArg)
 	int iBtn = IDOK;
 	#ifdef SHOW_INJECTREM_MSGBOX
 	wchar_t szDbgMsg[512], szTitle[128];
-	PROCESSENTRY32 pinf;
-	GetProcessInfo(nRemotePID, &pinf);
+	PROCESSENTRY32W pinf{};
+	GetProcessInfo(nRemotePID, pinf);
 	swprintf_c(szTitle, L"ConEmuCD PID=%u", GetCurrentProcessId());
 	swprintf_c(szDbgMsg, L"Hooking PID=%s {%s}\nConEmuCD PID=%u. Continue with injects?", asCmdArg ? asCmdArg : L"", pinf.szExeFile, GetCurrentProcessId());
 	iBtn = MessageBoxW(nullptr, szDbgMsg, szTitle, MB_SYSTEMMODAL|MB_OKCANCEL);
@@ -1236,14 +1236,9 @@ int __stdcall ConsoleMain3(const ConsoleMainMode anWorkMode, LPCWSTR asCmdLine)
 		}
 
 		LPSECURITY_ATTRIBUTES lpSec = nullptr; //LocalSecurity();
-		//#ifdef _DEBUG
-		//		lpSec = nullptr;
-		//#endif
-		// Не будем разрешать наследование, если нужно - сделаем DuplicateHandle
-		lbRc = createProcess(!gState.bSkipWowChange_, nullptr, gpszRunCmd, lpSec,lpSec, lbInheritHandle,
-		                      NORMAL_PRIORITY_CLASS/*|CREATE_NEW_PROCESS_GROUP*/
-		                      |CREATE_SUSPENDED/*((gpStatus->runMode_ == RunMode::RM_SERVER) ? CREATE_SUSPENDED : 0)*/,
-		                      nullptr, pszCurDir, &si, &pi);
+		lbRc = createProcess(gState.bSkipWowChange_, nullptr, gpszRunCmd, lpSec, lpSec, lbInheritHandle,
+			NORMAL_PRIORITY_CLASS/*|CREATE_NEW_PROCESS_GROUP*/ | CREATE_SUSPENDED/*((gpStatus->runMode_ == RunMode::RM_SERVER) ? CREATE_SUSPENDED : 0)*/,
+			nullptr, pszCurDir, &si, &pi);
 		dwErr = GetLastError();
 
 		if (!lbRc && (gState.runMode_ == RunMode::Server) && dwErr == ERROR_FILE_NOT_FOUND)
@@ -1265,12 +1260,10 @@ int __stdcall ConsoleMain3(const ConsoleMainMode anWorkMode, LPCWSTR asCmdLine)
 						*pszSlash = 0; // получили родительскую папку
 						pszCurDir = szSelf;
 						SetCurrentDirectory(pszCurDir);
-						// Пробуем еще раз, в родительской директории
-						// Не будем разрешать наследование, если нужно - сделаем DuplicateHandle
-						lbRc = createProcess(!gState.bSkipWowChange_, nullptr, gpszRunCmd, nullptr,nullptr, FALSE/*TRUE*/,
-						                      NORMAL_PRIORITY_CLASS/*|CREATE_NEW_PROCESS_GROUP*/
-						                      |CREATE_SUSPENDED/*((gpStatus->runMode_ == RunMode::RM_SERVER) ? CREATE_SUSPENDED : 0)*/,
-						                      nullptr, pszCurDir, &si, &pi);
+						// Try again in the up-level directory
+						lbRc = createProcess(gState.bSkipWowChange_, nullptr, gpszRunCmd, nullptr, nullptr, FALSE/*TRUE*/,
+							NORMAL_PRIORITY_CLASS/*|CREATE_NEW_PROCESS_GROUP*/ | CREATE_SUSPENDED/*((gpStatus->runMode_ == RunMode::RM_SERVER) ? CREATE_SUSPENDED : 0)*/,
+							nullptr, pszCurDir, &si, &pi);
 						dwErr = GetLastError();
 					}
 				}

src/ConEmuHk/ShellProcessor.cpp
@@ -3111,7 +3111,7 @@ void CShellProc::RunInjectHooks(LPCWSTR asFrom, PROCESS_INFORMATION *lpPI) const
 
 	if (iHookRc != CIH_OK/*0*/)
 	{
-		DWORD nErrCode = GetLastError();
+		const DWORD nErrCode = GetLastError();
 		// Хуки не получится установить для некоторых системных процессов типа ntvdm.exe,
 		// но при запуске dos приложений мы сюда дойти не должны
 		_ASSERTE(iHookRc == 0);

src/common/CmdLine_test.cpp
@@ -37,6 +37,8 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "MStrDup.h"
 #include "RConStartArgsEx.h"
 
+extern bool gbVerifyIgnoreAsserts;
+
 TEST(CmdLine, NextArg_Switches)
 {
 	LPCWSTR pszCmd =

src/common/MHandle.h
@@ -110,22 +110,22 @@ public:
 		return handle_;
 	}
 
-	HANDLE SetHandle(HANDLE handle) noexcept
+	bool SetHandle(HANDLE handle) noexcept
 	{
 		if (handle_ != handle)
 			Close();
 		handle_ = handle;
 		closeFunc_ = nullptr;
-		return handle_;
+		return HasHandle();
 	}
 
-	HANDLE SetHandle(HANDLE handle, std::function<void(HANDLE)> closeFunc) noexcept
+	bool SetHandle(HANDLE handle, std::function<void(HANDLE)> closeFunc) noexcept
 	{
 		if (handle_ != handle)
 			Close();
 		handle_ = handle;
 		closeFunc_ = std::move(closeFunc);
-		return handle_;
+		return HasHandle();
 	}
 
 	void Close() noexcept

src/common/WObjects.cpp
@@ -31,9 +31,12 @@ THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "defines.h"
 #include "CmdLine.h"
 #include "EnvVar.h"
+#include "MHandle.h"
 #include "MModule.h"
 #include "MStrDup.h"
 #include "WObjects.h"
+
+#include "MWow64Disable.h"
 
 #if !defined(CONEMU_MINIMAL)
 #include "WFiles.h"
@@ -69,57 +72,63 @@ bool FileExists(const wchar_t* asFilePath, uint64_t* pnSize /*= nullptr*/)
 
 	_ASSERTE(wcschr(asFilePath, L'\t')==nullptr);
 
-	WIN32_FIND_DATAW fnd{};
-	// ReSharper disable once CppLocalVariableMayBeConst
-	HANDLE hFind = FindFirstFile(asFilePath, &fnd);
 
-	if (hFind == INVALID_HANDLE_VALUE)
+	MHandle hFile;
+	MWow64Disable wow;
+	if (!hFile.SetHandle(CreateFileW(asFilePath, GENERIC_READ, FILE_SHARE_READ,
+		nullptr, OPEN_EXISTING, 0, nullptr), CloseHandle))
+	{
+		const DWORD nErrCode = GetLastError();
+		#ifndef _WIN64
+		// e.g. checking "C:\Windows\System32\wsl.exe" from 32-bit ConEmuC
+		if (nErrCode == ERROR_FILE_NOT_FOUND)
+		{
+			wow.Disable();
+			
+			hFile.SetHandle(CreateFileW(asFilePath, GENERIC_READ, FILE_SHARE_READ,
+				nullptr, OPEN_EXISTING, 0, nullptr), CloseHandle);
+		}
+		#endif
+		std::ignore = nErrCode;
+	}
+	if (hFile.HasHandle())
 	{
 		bool lbFileFound = false;
-
-		// FindFirstFile could fail on symlinks
-		const DWORD nErrCode = GetLastError();
-		if (nErrCode == ERROR_ACCESS_DENIED)
+		BY_HANDLE_FILE_INFORMATION fi{};
+		if (GetFileInformationByHandle(hFile, &fi) && !(fi.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
 		{
-			// ReSharper disable once CppLocalVariableMayBeConst
-			HANDLE hFile = CreateFile(asFilePath, GENERIC_READ, FILE_SHARE_READ, nullptr, OPEN_EXISTING, 0, nullptr);
-
-			if (hFile && hFile != INVALID_HANDLE_VALUE)
-			{
-				BY_HANDLE_FILE_INFORMATION fi{};
-
-				if (GetFileInformationByHandle(hFile, &fi) && !(fi.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY))
-				{
-					lbFileFound = true;
-
-					if (pnSize)
-						*pnSize = (static_cast<uint64_t>(fi.nFileSizeHigh) << 32) | static_cast<uint64_t>(fi.nFileSizeLow);
-				}
-
-				CloseHandle(hFile);
-			}
+			if (pnSize)
+				*pnSize = (static_cast<uint64_t>(fi.nFileSizeHigh) << 32) | static_cast<uint64_t>(fi.nFileSizeLow);
+			lbFileFound = true;
 		}
-
 		return lbFileFound;
 	}
+	wow.Restore();
+
 
 	bool lbFound = false;
+	// #TODO May we safely remove FindFirstFileW check and rely on CreateFileW only?
+	WIN32_FIND_DATAW fnd{};
+	const MHandle hFind(FindFirstFileW(asFilePath, &fnd), FindClose);
 
-	do
+	if (hFind.HasHandle())
 	{
-		if ((fnd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+		do
 		{
-			lbFound = true;
+			if ((fnd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0)
+			{
+				lbFound = true;
 
-			if (pnSize)
-				*pnSize = (static_cast<uint64_t>(fnd.nFileSizeHigh) << 32) | static_cast<uint64_t>(fnd.nFileSizeLow);
+				if (pnSize)
+					*pnSize = (static_cast<uint64_t>(fnd.nFileSizeHigh) << 32) | static_cast<uint64_t>(fnd.nFileSizeLow);
 
-			break;
-		}
+				_ASSERTE(FALSE && "CreateFile should be succeesful above");
+
+				break;
+			}
+		} while (FindNextFileW(hFind, &fnd));
 	}
-	while (FindNextFile(hFind, &fnd));
 
-	FindClose(hFind);
 	return lbFound;
 }
 

commit aafe04a6bddb74d13caf65652c2e224d1dc0854a

Internal. Fix `-new_console` regression from 82a2ca16b.

src/ConEmuCD/WorkerBase.cpp
@@ -336,6 +336,27 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		}
 	}
 
+	if (gState.runMode_ == RunMode::Comspec)
+	{
+		// New console was requested?
+		if (IsNewConsoleArg(lsCmdLine))
+		{
+			auto* comspec = dynamic_cast<WorkerComspec*>(gpWorker);
+			if (!comspec)
+			{
+				_ASSERTE(comspec != nullptr);
+			}
+			else
+			{
+				const auto iNewConRc = comspec->ProcessNewConsoleArg(lsCmdLine);
+				if (iNewConRc != 0)
+				{
+					return iNewConRc;
+				}
+			}
+		}
+	}
+
 	// Cut and process the "-new_console" / "-cur_console"
 	const CEStr stripNewConsole(lsCmdLine);
 	SafeFree(args_.pszSpecialCmd);
@@ -358,28 +379,7 @@ int WorkerBase::PostProcessPrepareCommandLine()
 		CStartEnvTitle setTitleVar(&gpszForcedTitle);
 		ProcessSetEnvCmd(lsCmdLine, pSetEnv, &setTitleVar);
 	}
-
-	if (gState.runMode_ == RunMode::Comspec)
-	{
-		// New console was requested?
-		if (IsNewConsoleArg(lsCmdLine))
-		{
-			auto* comspec = dynamic_cast<WorkerComspec*>(gpWorker);
-			if (!comspec)
-			{
-				_ASSERTE(comspec != nullptr);
-			}
-			else
-			{
-				const auto iNewConRc = comspec->ProcessNewConsoleArg(lsCmdLine);
-				if (iNewConRc != 0)
-				{
-					return iNewConRc;
-				}
-			}
-		}
-	}
-
+	
 	LPCWSTR pszArguments4EnvVar = nullptr;
 	CEStr szExeTest;
 