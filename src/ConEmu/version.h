864c2fe298b16f55986b84be720abd988f54d9c0
Maximus5 <ConEmu.Maximus5@gmail.com>
2021-01-29 00:30:02 +0100

    210128 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 210112
+// 210128
 #define MVV_1 21
 #define MVV_2 1
-#define MVV_3 12
+#define MVV_3 28
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 21,1,12,0
-#define CONEMUVERS "210112"
-#define CONEMUVERL L"210112"
-#define MSI86 "../ConEmu.210112.x86.msi"
-#define MSI64 "../ConEmu.210112.x64.msi"
+#define CONEMUVERN 21,1,28,0
+#define CONEMUVERS "210128"
+#define CONEMUVERL L"210128"
+#define MSI86 "../ConEmu.210128.x86.msi"
+#define MSI64 "../ConEmu.210128.x64.msi"
