3c49f77a2b17c01500d026cbada4c6d75121ab86
Maximus5 <ConEmu.Maximus5@gmail.com>
2021-03-02 22:36:07 +0100

    Internal. Move tests to separate tab

--- Release/UnitTests.cmd
+++ Release/UnitTests.cmd
@@ -10,11 +10,11 @@ if NOT "%~1" == "" (
 set ConEmu
 
 cd /d "%~dp0"
-start "ConEmu64" "%~dp0ConEmu64.exe" -log -basic -nocascade -wndX 340 -wndY 200 -wndW 135 -wndH 30 -FontDir "%~dp0UnitTests" -cmd cmd /k "%~0" in_gui
+start "ConEmu64" "%~dp0ConEmu64.exe" -log -basic -nocascade -wndX 340 -wndY 200 -wndW 150 -wndH 30 -FontDir "%~dp0UnitTests" -run cmd /k "%~0" in_gui
 goto :EOF
 
 :clear_env
-@echo %ESC%[1;31;40mClearing all ConEmu* environment variables%ESC%[0m
+@echo %ESC%[1;33;40mClearing all ConEmu* environment variables%ESC%[0m
 @FOR /F "usebackq delims==" %%i IN (`set ConEmu`) DO @call :clear_var %%i
 @goto :EOF
 
@@ -26,31 +26,36 @@ goto :EOF
 call RenameTab "Root"
 call cecho /Green "This is first tab, running new tab with two splits"
 echo on
-c:\windows\system32\cmd.exe -new_console /k "%~0" tab1
-c:\windows\syswow64\cmd.exe -new_console:s2TV /k "%~0" tab2
-c:\windows\syswow64\cmd.exe -new_console:b:h999 /k "%~0" tab3
-c:\windows\syswow64\cmd.exe -new_console:ba /k "%~0" tab4
-c:\windows\syswow64\cmd.exe -new_console:bt:"Far":C:"%FARHOME%\Far.exe" /c "%~0" tab5
-c:\windows\syswow64\cmd.exe -new_console:abP:"<Monokai>":t:Tab6 /k "%~0" tab6
-c:\windows\syswow64\cmd.exe -new_console:b:t:Tab7 /k "%~0" tab7
-c:\windows\syswow64\cmd.exe -new_console:b:t:Tab8 /k "%~0" tab8
+c:\windows\system32\cmd.exe -new_console /k "%~0" tests
+c:\windows\system32\cmd.exe -new_console /k "%~0" cmd64
+c:\windows\syswow64\cmd.exe -new_console:s3TV /k "%~0" cmd32
+c:\windows\syswow64\cmd.exe -new_console:b:h999 /k "%~0" ansi
+c:\windows\syswow64\cmd.exe -new_console:ba /k "%~0" multi
+c:\windows\syswow64\cmd.exe -new_console:bt:"Far":C:"%FARHOME%\Far.exe" /c "%~0" far
+c:\windows\syswow64\cmd.exe -new_console:abP:"<Monokai>":t:TabX /k "%~0" monokai
+c:\windows\syswow64\cmd.exe -new_console:b:t:VimX /k "%~0" vim
+c:\windows\syswow64\cmd.exe -new_console:b:t:BashX /k "%~0" wsl
+goto fin
+
+:tests
+call RenameTab "Tests"
 rem Source cl tests
 if exist "%~dp0..\src\UnitTests\run-tests.cmd" call "%~dp0..\src\UnitTests\run-tests.cmd"
 goto fin
 
-:tab1
-call RenameTab "Tab1"
+:cmd64
+call RenameTab "cmd64"
 call cecho /Green "This is tab1 running x64 cmd"
 set Progr
 goto fin
 
-:tab2
-call RenameTab "Tab2"
+:cmd32
+call RenameTab "cmd32"
 call cecho /Yellow "This is tab2 running x32 cmd"
 set Progr
 goto fin
 
-:tab3
+:ansi
 rem This test for 256color and SEVERAL pause/long line prints
 call RenameTab "Ansi"
 type "%ConEmuDir%\UnitTests\AnsiColors256.ans"
@@ -59,8 +64,8 @@ pause
 pause
 goto fin
 
-:tab4
-call RenameTab "Test"
+:multi
+call RenameTab "multi"
 cd /d "%~dp0UnitTests"
 MultiRun.exe /exit
 echo Errorlevel=%errorlevel%
@@ -76,22 +81,22 @@ MultiRunX64.exe
 echo Errorlevel=%errorlevel%
 goto fin
 
-:tab5
+:far
 echo This tab starts Far Manager asynchronously.
 echo Root cmd.exe is expected to terminate normally.
 conemuc /async /c "%FARHOME%\Far.exe" /w- /x /p"%ConEmuDir%\Plugins\ConEmu;%FARHOME%\Plugins"
 goto :EOF
 
-:tab6
+:monokai
 echo This tab is elevated cmd.exe started with ^<Monokai^> color scheme.
 goto :EOF
 
-:tab7
+:vim
 cd /d %~d0\Utils\Alternative\ViM74
 call vx.cmd
 goto :EOF
 
-:tab8
+:wsl
 ConEmuC64.exe -c {bash}
 goto :EOF
 
