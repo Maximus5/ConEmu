dead3d3fc557819ef91f077690212b6331f1aa79
Maximus5 <ConEmu.Maximus5@gmail.com>
2023-07-24 01:17:15 +0200

    230724 release files

--- src/ConEmu/version.h
+++ src/ConEmu/version.h
@@ -1,7 +1,7 @@
-// 230723
+// 230724
 #define MVV_1 23
 #define MVV_2 7
-#define MVV_3 23
+#define MVV_3 24
 #define MVV_4 0
 #define MVV_4a ""
 #undef MVV_git
--- src/Setup/Setupper/VersionI.h
+++ src/Setup/Setupper/VersionI.h
@@ -1,5 +1,5 @@
-#define CONEMUVERN 23,7,23,0
-#define CONEMUVERS "230723"
-#define CONEMUVERL L"230723"
-#define MSI86 "../ConEmu.230723.x86.msi"
-#define MSI64 "../ConEmu.230723.x64.msi"
+#define CONEMUVERN 23,7,24,0
+#define CONEMUVERS "230724"
+#define CONEMUVERL L"230724"
+#define MSI86 "../ConEmu.230724.x86.msi"
+#define MSI64 "../ConEmu.230724.x64.msi"
